05909fc042a17ec63a7dda4cd98c6967fcd0075b
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2022-10-26 20:38:37 -0500

    content: add new scars

--- scripts/cat/appearance_utility.py
+++ scripts/cat/appearance_utility.py
@@ -199,6 +199,8 @@ def init_scars(cat):
         ])
     else:
         cat.specialty = None
+    if cat.specialty == 'NOTAIL' and cat.specialty2 == 'HALFTAIL':
+        cat.specialty2 = None
 
     scar_choice2 = randint(0, 30)
     if cat.age in ['kitten', 'adolescent']:
@@ -214,6 +216,8 @@ def init_scars(cat):
         ])
     else:
         cat.specialty2 = None
+    if cat.specialty2 == 'NOTAIL' and cat.specialty == 'HALFTAIL':
+        cat.specialty == None
 
 def init_accessories(cat):
     acc_display_choice = randint(0, 35)
