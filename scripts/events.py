2ec1c1bc95e1b2e24e3ad13c78d26e9ae655b4e7
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2022-10-28 12:02:48 -0500

    bugfix: maybe actually fixed medcat issue now

--- scripts/events.py
+++ scripts/events.py
@@ -220,7 +220,7 @@ class Events(object):
                     else:
                         chance = randint(0, 30)
                     print('POSSIBLE MED APP - CHANCE:', chance)
-                    if chance in range(0, 11):    
+                    if chance in range(1, 11):    
                         if cat.trait in ['polite', 'quiet', 'sweet', 'daydreamer']:
                             chance = 1
                         else:
