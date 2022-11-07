b311fb19a50282fa89572148e8e5e2718f0a93d7
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2022-11-06 20:38:00 -0600

    Cats are now not able to be given a blank name FIxed spacing issues

--- scripts/events.py
+++ scripts/events.py
@@ -950,51 +950,51 @@ class Events():
                 other_name = str(other_cat.name)
                 if game.clan.current_season in ['Leaf-fall', 'Leaf-bare']:
                     cause_of_death = [
-                        name + ' lost a life due to greencough ',
-                        name + ' lost a life due to whitecough ',
-                        name + ' lost a life due to yellowcough ',
+                        name + ' lost a life due to greencough',
+                        name + ' lost a life due to whitecough',
+                        name + ' lost a life due to yellowcough',
                     ]
                 else:
                     cause_of_death = [
-                        name + ' lost a life after falling into a river ',
-                        'Lightning fell in camp and ' + name + ' lost a life ',
-                        name + ' was mortally wounded by a fox ',
-                        name + ' lost a life to a dog ',
-                        name + ' lost a life to a badger ',
-                        name + ' lost a life to a hawk ',
-                        name + ' lost a life while fighting off a rogue ',
-                        name + ' lost a life to an eagle ',
-                        name + ' was grabbed and dropped by an eagle, losing a life ',
-                        name + ' was grabbed and dropped by a hawk, losing a life ',
-                        name + ' lost a life after being swept away by a flood ',
-                        name + ' lost a life after falling off a tree ',
-                        name + ' was bit by a venomous spider and lost a life ',
-                        name + ' was bit by a venomous snake and lost a life ',
-                        name + ' ate poisoned fresh-kill and lost a life ',
-                        name + ' failed to interpret a warning sign from StarClan and lost a life as a result ',
-                        name + ' lost a life defending ' + other_name + ' from a dog ',
-                        name + ' lost a life defending ' + other_name + ' from a badger ',
-                        name + ' lost a life defending ' + other_name + ' from a fox ',
-                        name + ' lost a life defending ' + other_name + ' from a hawk ',
-                        name + ' lost a life defending ' + other_name + ' from an eagle ',
-                        name + ' lost a life while saving ' + other_name + ' from drowning ',
-                        name + ' lost a life while saving ' + other_name + ' from a monster ',
-                        name + ' was pushed under a monster and lost a life ',
-                        name + ' lost a life after saving ' + other_name + ' from a snake '
+                        name + ' lost a life after falling into a river',
+                        'Lightning fell in camp and ' + name + ' lost a life',
+                        name + ' was mortally wounded by a fox',
+                        name + ' lost a life to a dog',
+                        name + ' lost a life to a badger',
+                        name + ' lost a life to a hawk',
+                        name + ' lost a life while fighting off a rogue',
+                        name + ' lost a life to an eagle',
+                        name + ' was grabbed and dropped by an eagle, losing a life',
+                        name + ' was grabbed and dropped by a hawk, losing a life',
+                        name + ' lost a life after being swept away by a flood',
+                        name + ' lost a life after falling off a tree',
+                        name + ' was bit by a venomous spider and lost a life',
+                        name + ' was bit by a venomous snake and lost a life',
+                        name + ' ate poisoned fresh-kill and lost a life',
+                        name + ' failed to interpret a warning sign from StarClan and lost a life as a result',
+                        name + ' lost a life defending ' + other_name + ' from a dog',
+                        name + ' lost a life defending ' + other_name + ' from a badger',
+                        name + ' lost a life defending ' + other_name + ' from a fox',
+                        name + ' lost a life defending ' + other_name + ' from a hawk',
+                        name + ' lost a life defending ' + other_name + ' from an eagle',
+                        name + ' lost a life while saving ' + other_name + ' from drowning',
+                        name + ' lost a life while saving ' + other_name + ' from a monster',
+                        name + ' was pushed under a monster and lost a life',
+                        name + ' lost a life after saving ' + other_name + ' from a snake'
                     ]
 
                     
                 if len(game.clan.all_clans) > 0:
                     cause_of_death.extend([
                         name + ' lost a life defending the kits from ' +
-                        choice(game.clan.all_clans).name + 'Clan warriors ',
+                        choice(game.clan.all_clans).name + 'Clan warriors',
                         name + ' lost a life defending ' + other_name +
                         ' from ' + choice(game.clan.all_clans).name +
-                        'Clan warriors ',
+                        'Clan warriors',
                         name + ' lost a life to a ' +
-                        choice(game.clan.all_clans).name + 'Clan apprentice ',
+                        choice(game.clan.all_clans).name + 'Clan apprentice',
                         name + ' lost a life to a ' +
-                        choice(game.clan.all_clans).name + 'Clan warrior '
+                        choice(game.clan.all_clans).name + 'Clan warrior'
                     ])
                 game.clan.leader_lives -= 1
                 cat.die()
@@ -1080,54 +1080,54 @@ class Events():
         elif randint(1, 400) == 1:
             name = str(cat.name)
             cause_of_death = [
-                name + ' was murdered ',
-                'A tree fell in camp and killed ' + name + ' ',
-                name + ' was found dead near a fox den ',
-                name + ' was bitten by a snake and died '
+                name + ' was murdered',
+                'A tree fell in camp and killed ' + name,
+                name + ' was found dead near a fox den',
+                name + ' was bitten by a snake and died'
             ]
             if clan_has_kits and cat.status != 'kitten':
                 cause_of_death.extend([
-                    name + ' was bitten by a snake while saving a kit and died '
+                    name + ' was bitten by a snake while saving a kit and died'
                 ])
             if cat.status == 'kitten':
                 cause_of_death.extend([
-                    name + ' fell into a river and drowned ',
-                    name + ' was taken by a hawk ',
-                    name + ' grew weak as the days passed and died ',
-                    name + ' was killed after sneaking out of camp ',
-                    name + ' died after accidentally eating deathberries ',
+                    name + ' fell into a river and drowned',
+                    name + ' was taken by a hawk',
+                    name + ' grew weak as the days passed and died',
+                    name + ' was killed after sneaking out of camp',
+                    name + ' died after accidentally eating deathberries',
                     name +
-                    ' was killed in their sleep after a snake snuck into camp '
+                    ' was killed in their sleep after a snake snuck into camp'
                 ])
                 if game.clan.current_season == 'Leaf-bare':
                     cause_of_death.extend([
-                        name + ' was found dead in the snow ',
-                        name + ' froze to death in a harsh snowstorm ', name +
-                        ' disappeared from the nursery and was found dead in the territory ',
+                        name + ' was found dead in the snow',
+                        name + ' froze to death in a harsh snowstorm', name +
+                        ' disappeared from the nursery and was found dead in the territory',
                         name +
-                        ' was playing on the ice when the ice cracked and they drowned ',
-                        name + ' died of greencough '
+                        ' was playing on the ice when the ice cracked and they drowned',
+                        name + ' died of greencough'
                     ])
                     if cat.status == 'kitten':
                         cause_of_death.extend([
                         name + ' died of kittencough ', name +
-                        ' was too weak to fight off whitecough and passed ',
-                        name + ' caught a cold and slowly faded '
+                        ' was too weak to fight off whitecough and passed',
+                        name + ' caught a cold and slowly faded'
                     ])
                     if cat.status == 'elder' or cat.moons < 150:
                         cause_of_death.extend([
-                        name + ' was already feeling weak and a case of whitecough finished them off ',
-                        name + ' was too weak to recover from an illness and passed ',
-                        'Weakened by a lack of prey, ' + name + ' couldn\'t fight off whitecough and passed ',
-                        name + ' was taken quickly by a case of greencough '
+                        name + ' was already feeling weak and a case of whitecough finished them off',
+                        name + ' was too weak to recover from an illness and passed',
+                        'Weakened by a lack of prey, ' + name + ' couldn\'t fight off whitecough and passed',
+                        name + ' was taken quickly by a case of greencough'
                     ])
                 if game.clan.current_season == 'Greenleaf':
-                    cause_of_death.extend([name + ' died to overheating '])
+                    cause_of_death.extend([name + ' died to overheating'])
             elif cat.status == 'apprentice':
                 cause_of_death.extend([
-                    name + ' died in a training accident ',
-                    name + ' was killed by enemy warriors after accidentally wandering over the border ',
-                    name + ' went missing and was found dead ',
+                    name + ' died in a training accident',
+                    name + ' was killed by enemy warriors after accidentally wandering over the border',
+                    name + ' went missing and was found dead',
                     name + ' died in a border skirmish'
                 ])
                 if game.clan.biome == "Mountainous":
