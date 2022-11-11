ee8521ad3286aee8cc290f3df790f213ff6cab98
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2022-11-11 14:55:05 -0600

    quick bug fix

--- scripts/events.py
+++ scripts/events.py
@@ -844,6 +844,7 @@ class Events():
             #return
 
         # get the general information about the cat and a random other cat
+        cause_of_death = []
         triggered_death = False
         name = str(cat.name)
         other_cat = choice(list(Cat.all_cats.values()))
@@ -1311,6 +1312,7 @@ class Events():
             #return
     
         # get the general information about the cat and a random other cat
+        cause_of_death = []
         triggered_death = False
         other_cat = choice(list(Cat.all_cats.values()))
         countdown = int(len(Cat.all_cats) / 3)
