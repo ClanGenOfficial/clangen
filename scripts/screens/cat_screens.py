389ee92e305de56d4b55774921ec0470ebc1ca7a
Lixxis <lixxis.arts@gmail.com>
2022-11-26 18:57:30 +0100

    bugfix: change last med to warrior did not save

--- scripts/screens/cat_screens.py
+++ scripts/screens/cat_screens.py
@@ -1090,6 +1090,14 @@ class ProfileScreen(Screens):
                     'apprentice_switch'].status == 'medicine cat':
             game.switches['apprentice_switch'].status_change('warrior')
             game.switches['apprentice_switch'] = False
+            clan_still_has_med = list(filter(
+                lambda c: c.status == 'medicine cat' and not c.dead and not c.exiled
+                , Cat.all_cats.values()
+            ))
+            if not clan_still_has_med:
+                game.clan.medicine_cat = None
+            else:
+                game.clan.medicine_cat = clan_still_has_med[0]
 
         # ---------------------------------------------------------------------------- #
         #                                 PERSONAL TAB                                 #
