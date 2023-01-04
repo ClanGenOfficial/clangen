e90166e99395639dadd7d9a850f727946d6a8110
scribblecrumb <abspence2199@gmail.com>
2023-01-04 13:56:09 -0600

    crash

--- scripts/screens/cat_screens.py
+++ scripts/screens/cat_screens.py
@@ -604,13 +604,14 @@ class ProfileScreen(Screens):
         self.profile_elements["cat_image"].disable()
 
         # if cat is a med or med app, show button for their den
-        if self.the_cat.status in ['medicine cat', 'medicine cat apprentice']:
-            self.profile_elements["med_den"] = UIImageButton(pygame.Rect
-                                                             ((100, 380), (151, 28)),
-                                                             "",
-                                                             object_id="#med_den_button"
-            )
-
+        self.profile_elements["med_den"] = UIImageButton(pygame.Rect
+                                                         ((100, 380), (151, 28)),
+                                                         "",
+                                                         object_id="#med_den_button"
+        )
+        if self.the_cat.status not in ['medicine cat', 'medicine cat apprentice']:
+            self.profile_elements["med_den"].hide()
+            
         # Determine where the next and previous cat buttons lead
         self.determine_previous_and_next_cat()
 
