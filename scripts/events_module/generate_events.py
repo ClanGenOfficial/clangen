7a6e1df96ba1aaefd8998d95848c9859a7d893b6
Alana Huang <alana.y.huang@gmail.com>
2023-01-06 17:06:45 +1100

    better err detection cont."

--- scripts/events_module/generate_events.py
+++ scripts/events_module/generate_events.py
@@ -3,8 +3,8 @@ from scripts.game_structure.game_essentials import game
 
 resource_directory = "resources/dicts/events/"
 
-class GenerateEvents():
 
+class GenerateEvents:
     @staticmethod
     def get_event_dicts(event_triggered, cat_type, biome):
         events = None
@@ -25,24 +25,54 @@ class GenerateEvents():
         if cat_type in ["medicine cat", "medicine cat apprentice"]:
             cat_type = "medicine"
 
-        event_list.extend(self.generate_injury_event(GenerateEvents.get_event_dicts("injury", cat_type, "general")))
+        event_list.extend(
+            self.generate_injury_event(
+                GenerateEvents.get_event_dicts("injury", cat_type, "general")
+            )
+        )
 
         # skip the rest of the loading if there is an unrecognised cat type
         if cat_type not in game.clan.CAT_TYPES:
-            print(f"WARNING: unrecognised cat status {cat_type} in generate_events. Have you added it to CAT_TYPES in clan.py?")
+            print(
+                f"WARNING: unrecognised cat status {cat_type} in generate_events. Have you added it to CAT_TYPES in clan.py?"
+            )
 
         elif game.clan.biome not in game.clan.BIOME_TYPES:
-            print(f"WARNING: unrecognised biome {game.clan.biome} in generate_events. Have you added it to BIOME_TYPES in clan.py?")
+            print(
+                f"WARNING: unrecognised biome {game.clan.biome} in generate_events. Have you added it to BIOME_TYPES in clan.py?"
+            )
 
-        else:   
-            event_list.extend(self.generate_injury_event(GenerateEvents.get_event_dicts("injury", cat_type, game.clan.biome.lower())))
+        else:
+            event_list.extend(
+                self.generate_injury_event(
+                    GenerateEvents.get_event_dicts(
+                        "injury", cat_type, game.clan.biome.lower()
+                    )
+                )
+            )
 
             if cat_type in ["apprentice", "deputy", "leader"]:
-                event_list.extend(self.generate_injury_event(GenerateEvents.get_event_dicts("injury", "warrior", game.clan.biome.lower())))
-            
-            if cat_type not in ['kitten', 'leader']:
-                event_list.extend(self.generate_injury_event(GenerateEvents.get_event_dicts("injury", "general", "general")))
-                event_list.extend(self.generate_injury_event(GenerateEvents.get_event_dicts("injury", "general", game.clan.biome.lower())))
+                event_list.extend(
+                    self.generate_injury_event(
+                        GenerateEvents.get_event_dicts(
+                            "injury", "warrior", game.clan.biome.lower()
+                        )
+                    )
+                )
+
+            if cat_type not in ["kitten", "leader"]:
+                event_list.extend(
+                    self.generate_injury_event(
+                        GenerateEvents.get_event_dicts("injury", "general", "general")
+                    )
+                )
+                event_list.extend(
+                    self.generate_injury_event(
+                        GenerateEvents.get_event_dicts(
+                            "injury", "general", game.clan.biome.lower()
+                        )
+                    )
+                )
 
         return event_list
 
@@ -57,28 +87,65 @@ class GenerateEvents():
                 cat_trait=event["cat_trait"],
                 cat_skill=event["cat_skill"],
                 other_cat_trait=event["other_cat_trait"],
-                other_cat_skill=event["other_cat_skill"]
+                other_cat_skill=event["other_cat_skill"],
             )
             injury_list.append(injury_event)
         return injury_list
-    
+
     def possible_death_events(self, cat_type, age):
 
         event_list = []
         if cat_type in ["medicine cat", "medicine cat apprentice"]:
             cat_type = "medicine"
-            
-        event_list.extend(self.generate_death_events(GenerateEvents.get_event_dicts("death", cat_type, "general")))
 
-        if game.clan.biome in BIOME_TYPES:
-            event_list.extend(self.generate_death_events(GenerateEvents.get_event_dicts("death", cat_type, game.clan.biome.lower())))
+        event_list.extend(
+            self.generate_death_events(
+                GenerateEvents.get_event_dicts("death", cat_type, "general")
+            )
+        )
 
-        if cat_type in ["apprentice", "deputy", "leader"]:
-            event_list.extend(self.generate_death_events(GenerateEvents.get_event_dicts("death", "warrior", game.clan.biome.lower())))
-        
-        if cat_type not in ['kitten', 'leader']:
-            event_list.extend(self.generate_death_events(GenerateEvents.get_event_dicts("death", "general", "general")))
-            event_list.extend(self.generate_death_events(GenerateEvents.get_event_dicts("death", "general", game.clan.biome.lower())))
+        # skip the rest of the loading if there is an unrecognised cat type
+        if cat_type not in game.clan.CAT_TYPES:
+            print(
+                f"WARNING: unrecognised cat status {cat_type} in generate_events. Have you added it to CAT_TYPES in clan.py?"
+            )
+
+        elif game.clan.biome not in game.clan.BIOME_TYPES:
+            print(
+                f"WARNING: unrecognised biome {game.clan.biome} in generate_events. Have you added it to BIOME_TYPES in clan.py?"
+            )
+
+        else:
+            event_list.extend(
+                self.generate_death_events(
+                    GenerateEvents.get_event_dicts(
+                        "death", cat_type, game.clan.biome.lower()
+                    )
+                )
+            )
+
+            if cat_type in ["apprentice", "deputy", "leader"]:
+                event_list.extend(
+                    self.generate_death_events(
+                        GenerateEvents.get_event_dicts(
+                            "death", "warrior", game.clan.biome.lower()
+                        )
+                    )
+                )
+
+            if cat_type not in ["kitten", "leader"]:
+                event_list.extend(
+                    self.generate_death_events(
+                        GenerateEvents.get_event_dicts("death", "general", "general")
+                    )
+                )
+                event_list.extend(
+                    self.generate_death_events(
+                        GenerateEvents.get_event_dicts(
+                            "death", "general", game.clan.biome.lower()
+                        )
+                    )
+                )
 
         return event_list
 
@@ -93,23 +160,26 @@ class GenerateEvents():
                 cat_trait=event["cat_trait"],
                 cat_skill=event["cat_skill"],
                 other_cat_trait=event["other_cat_trait"],
-                other_cat_skill=event["other_cat_skill"]
+                other_cat_skill=event["other_cat_skill"],
             )
             death_list.append(death_event)
 
         return death_list
 
+
 class InjuryEvent:
-    def __init__(self,
-                 injury=None,
-                 camp='any',
-                 tags=None,
-                 event_text='',
-                 history_text=None,
-                 cat_trait=None,
-                 cat_skill=None,
-                 other_cat_trait=None,
-                 other_cat_skill=None):
+    def __init__(
+        self,
+        injury=None,
+        camp="any",
+        tags=None,
+        event_text="",
+        history_text=None,
+        cat_trait=None,
+        cat_skill=None,
+        other_cat_trait=None,
+        other_cat_skill=None,
+    ):
         self.injury = injury
         self.camp = camp
         self.tags = tags
@@ -122,15 +192,17 @@ class InjuryEvent:
 
 
 class DeathEvent:
-    def __init__(self,
-                 camp,
-                 tags=None,
-                 death_text='',
-                 history_text=None,
-                 cat_trait=None,
-                 cat_skill=None,
-                 other_cat_trait=None,
-                 other_cat_skill=None):
+    def __init__(
+        self,
+        camp,
+        tags=None,
+        death_text="",
+        history_text=None,
+        cat_trait=None,
+        cat_skill=None,
+        other_cat_trait=None,
+        other_cat_skill=None,
+    ):
         self.camp = camp
         self.tags = tags
         self.death_text = death_text
@@ -202,4 +274,3 @@ Use these to determine what corruption level the main cat should have, if releva
 You can mix and match corruption tags if you feel an event is on the line between two of them, the code will allow the event for a cat with either of the corruption levels tagged
 
 """
-
