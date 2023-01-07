38481801c0f1c3a10fead72feef107afa4418ad7
keyraven <118307471+keyraven@users.noreply.github.com>
2023-01-06 17:25:10 -0700

    Content: Tint
    Cats can now generate with a random color-tint that will effect it's pelt color.

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -1,9 +1,11 @@
 import pygame
 
 from scripts.game_structure.game_essentials import *
+import ujson
 
 
 class Sprites():
+    cat_tints = {}
 
     def __init__(self, original_size, new_size=None):
         self.size = original_size  # size of a single sprite in a spritesheet
@@ -12,6 +14,15 @@ class Sprites():
         self.images = {}
         self.groups = {}
         self.sprites = {}
+
+        self.load_tints()
+
+    def load_tints(self):
+        try:
+            with open("sprites/dicts/tint.json", 'r') as read_file:
+                Sprites.cat_tints = ujson.loads(read_file.read())
+        except:
+            print("Error Reading Tints")
 
     def spritesheet(self, a_file, name):
         """
