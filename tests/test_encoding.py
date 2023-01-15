3331ec2a22ffab81373793c82409aaa170238207
Dpeta <69427753+Dpeta@users.noreply.github.com>
2023-01-15 08:46:15 +0100

    Print diff for encoding test

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -6,37 +6,38 @@ This test isn't because we can't use utf-8 characters, but because we shouldn't
 use them accidentally without explicitly setting the encoding.
 
 Suggested commands to find the offending character:
-file -i file.txt
+file -i old_file.txt
 iconv -cf utf-8 -t ascii -o old_file.txt new_file.txt
 diff old_file.txt new_file.txt"""
 import os
 import sys
+import difflib
 
 
 def test():
     """Iterate through all files in 'resources'
     and verify all characters are ascii decodable."""
+    output = ""
     for (root, _, files) in os.walk("."):
         for file in files:
             if file.endswith(".json") or file.endswith(".py"):
                 path = os.path.join(root, file)
-
                 with open(path, "r", encoding="utf-8") as handle_utf8:
-                    utf_read = handle_utf8.read()
+                    utf_read = handle_utf8.readlines()
+                with open(path, "r", encoding="ascii", errors="replace") as handle_ascii:
+                    ascii_read = handle_ascii.readlines()
 
-                with open(path, "r", encoding="ascii") as handle_ascii:
-                    ascii_read = ""
-                    for _ in range(len(utf_read)):
-                        try:
-                            ascii_read += handle_ascii.read(1)
-                        except UnicodeDecodeError:
-                            line = ascii_read.count("\n")
-                            ascii_len = len(ascii_read)
-                            snippet = ascii_read[ascii_len - 20 : ascii_len + 20]
-                            sys.exit(f"Failed decode line {line} of {path}\n"
-                                     f"Last valid snippet: \"{ascii_read[-50:]}\"")
-
-                print(f"VALID: {path}")
+                # Get difference
+                result = difflib.context_diff(utf_read, ascii_read)
+                if result:
+                    tmp_output = ""
+                    for diff in result:  # Exit if the reads differ
+                        tmp_output += diff
+                    if tmp_output:
+                        output += f"\nFile {path} isn't ascii decodable!!\n"
+                        output += tmp_output
+    if output.strip():
+        sys.exit(output)
 
 
 if __name__ == "__main__":
