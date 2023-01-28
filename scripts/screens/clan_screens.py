9d127ad69b62edba3be0248dec3da252e1eb79ee
keyraven <118307471+keyraven@users.noreply.github.com>
2023-01-28 16:06:53 -0700

    Bugfixes

--- scripts/screens/clan_screens.py
+++ scripts/screens/clan_screens.py
@@ -41,15 +41,6 @@ class ClanScreen(Screens):
             elif game.clan.current_season == 'Leaf-fall':
                 screen.blit(self.leaffall_bg, (0, 0))
 
-        '''verdana.text("Leader\'s Den", game.clan.cur_layout['leader den'])
-        verdana.text('Medicine Cat Den', game.clan.cur_layout['medicine den'])
-        verdana.text('Nursery', game.clan.cur_layout['nursery'])
-        verdana.text('Clearing', game.clan.cur_layout['clearing'])
-        verdana.text("Apprentices\' Den",
-                     game.clan.cur_layout['apprentice den'])
-        verdana.text("Warriors\' Den", game.clan.cur_layout['warrior den'])
-        verdana.text("Elders\' Den", game.clan.cur_layout['elder den'])'''
-
     def handle_event(self, event):
         if event.type == pygame_gui.UI_BUTTON_START_PRESS:
             if event.ui_element == self.save_button:
@@ -850,8 +841,8 @@ class DFScreen(Screens):
             for cat in self.chunks(self.current_listed_cats, 20)[self.list_page - 1]:
                 update_sprite(cat)
                 self.display_cats.append(
-                    UISpriteButton(pygame.Rect
-                                   ((260 + pos_x, 360 + pos_y), (100, 100)),
+                    UISpriteButton(scale(pygame.Rect
+                                   ((260 + pos_x, 360 + pos_y), (100, 100))),
                                    cat.big_sprite,
                                    cat.ID,
                                    starting_height=1))
