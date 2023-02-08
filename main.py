60b3972baa7e549ad589fcd26c52ff575f8644f3
Lara <63805048+larkgz@users.noreply.github.com>
2023-02-07 23:05:20 -0500

    feat: Log error for files that fail to load

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
 import os
-import traceback
 directory = os.path.dirname(__file__)
 if directory:
     os.chdir(directory)
@@ -45,7 +44,7 @@ if clan_list:
         load_cats()
         clan_class.load_clan()
     except Exception as e:
-        print("ERROR: \n",traceback.format_exc())
+        logging.exception("File failed to load")
         if not game.switches['error_message']:
             game.switches[
                 'error_message'] = 'There was an error loading the cats file!'
