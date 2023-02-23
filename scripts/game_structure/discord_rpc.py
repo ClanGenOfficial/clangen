15e9cb6068098b6c551e50c77642600acdeb7ff3
ImLvna <44179559+ImLvna@users.noreply.github.com>
2023-02-22 21:34:05 -0500

    Make it more asthetically pleasing :cutie:

--- scripts/game_structure/discord_rpc.py
+++ scripts/game_structure/discord_rpc.py
@@ -7,6 +7,7 @@ If you do not have the pypresence module installed,
 this file will not be used, and the game will run as normal.
 """
 from time import time
+import os
 from scripts.game_structure.game_essentials import game
 
 
@@ -35,6 +36,7 @@ class _DiscordRPC():
             print("Discord RPC is supported")
         except ImportError:
             print("Pypresence not installed, Discord RPC isn't supported.")
+            print("To enable rpc, run 'pip install pypresence' in your terminal.")
             return
         # Check if Discord is running.
         try:
@@ -45,13 +47,12 @@ class _DiscordRPC():
             return
         # Try to connect.
         try:
+            self.rpc_supported = True
             self.connect()
             print("Connected to discord!")
         except ConnectionError as e:
             print(f"Failed to connect to Discord: {e}")
             return
-        # Only set when we're sure it works :O
-        self.rpc_supported = True
 
     def connect(self):
         if self.rpc_supported:
@@ -66,6 +67,16 @@ class _DiscordRPC():
             except KeyError:
                 state_text = "Leading the clan"
 
+            try:
+                img_str = f"{game.clan.biome}_{game.clan.current_season.replace('-', '')}_{game.clan.camp_bg}_{'dark' if game.settings['dark mode'] else 'light'}"
+                img_text = game.clan.biome
+            except AttributeError:
+                print("Failed to get image string, game may not be fully loaded yet. Dont worry, it will fix itself. Hopefully.")
+                img_str = "discord" # fallback incase the game isn't loaded yet
+                img_text = "Clangen!!"
+            
+            # Example: beach_greenleaf_camp1_dark
+
             clan_name = 'Loading...'
             cats_amount = 0
             if game.clan:
@@ -74,8 +85,10 @@ class _DiscordRPC():
             self.rpc.update(
                 state=state_text,
                 details="Managing " + clan_name,
-                large_image="170",
-                large_text=f"Managing {cats_amount} cats",
+                large_image=img_str.lower(),
+                large_text=img_text,
+                small_image="discord",
+                small_text=f"Managing {cats_amount} cats",
                 start=self.start_time
             )
 
