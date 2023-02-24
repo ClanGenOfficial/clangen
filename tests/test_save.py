b15fa868078e394c40e2e30863ad8f443efc59c5
ImLvna <imlvnaa@gmail.com>
2023-02-24 12:40:10 -0500

    Test if save is migrated

--- tests/test_save.py
+++ tests/test_save.py
@@ -53,6 +53,9 @@ class LoadSave(unittest.TestCase):
             with self.subTest(i=i):
                 print("Checking current clan for save " + str(i))
                 self.example_save(i)
+                fileList = os.listdir('saves')
+                if 'currentclan.txt' in fileList:
+                    self.skipTest("Save " + str(i) + " already migrated")
                 old_out = self.old_implimentation()
                 self.example_save(i)
                 new_out = self.new_implimentation()
@@ -68,8 +71,54 @@ class LoadSave(unittest.TestCase):
             with self.subTest(i=i):
                 print("Checking clan list for save " + str(i))
                 self.example_save(i)
+                fileList = os.listdir('saves')
+                if 'currentclan.txt' in fileList:
+                    self.skipTest("Save " + str(i) + " already migrated")
                 old_out = self.old_implimentation().sort()
                 self.example_save(i)
                 new_out = self.new_implimentation().sort()
 
                 self.assertEqual(old_out, new_out, "Clan list not saved correctly for save " + str(i))
+
+@unittest.skipIf(num_example_saves == 0, "No example saves found. Run 'git submodule update --init --recursive' to download example saves")
+class MigrateSave(unittest.TestCase):
+    def setUp(self):
+        if os.path.exists('saves'):
+            shutil.move('saves', 'saves_backup')
+
+    def tearDown(self):
+        if os.path.exists('saves'):
+            shutil.rmtree('saves')
+        if os.path.exists('saves_backup'):
+            shutil.move('saves_backup', 'saves')
+
+    def example_save(self, id):
+        if os.path.exists('saves'):
+            shutil.rmtree('saves')
+
+        #copy tests/testSaves/save<id> to saves
+        shutil.copytree('tests/testSaves/save' + str(id), 'saves')
+
+    def test_migrate_save_onread(self):
+        
+        for i in range(1, num_example_saves + 1):
+            with self.subTest(i=i):
+                print("Checking migration for save " + str(i))
+                self.example_save(i)
+                fileList = os.listdir('saves')
+                if 'currentclan.txt' in fileList:
+                    self.skipTest("Save " + str(i) + " already migrated")
+                
+                with open('saves/clanlist.txt', 'r') as read_file:
+                    clan_name = read_file.read().strip().splitlines()[0]
+
+                Game().read_clans() # the load save function should migrate the save
+
+                fileList = os.listdir('saves')
+                self.assertIn('currentclan.txt', fileList, "Save " + str(i) + " not migrated")
+
+                with open('saves/currentclan.txt', 'r') as read_file:
+                    curclan = read_file.read().strip()
+
+                self.assertEqual(curclan, clan_name, "Save " + str(i) + " not migrated correctly")
+                self.assertNotIn('clanlist.txt', fileList, "Save " + str(i) + " not migrated correctly")
