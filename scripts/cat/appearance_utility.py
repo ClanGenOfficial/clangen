f28b74b6d0bfc732c958f779a0405e125a7d12b2
THybrid21 <124342283+THybrid21@users.noreply.github.com>
2023-03-12 00:47:17 +1100

    Equalise Pelts
    Set it so all the pelts were equal odds of appearing on generated cats...
    
    Also tripled pride odds.

--- scripts/cat/appearance_utility.py
+++ scripts/cat/appearance_utility.py
@@ -487,9 +487,7 @@ def randomize_pelt(cat):
     #   PELT COLOUR
     # ------------------------------------------------------------------------------------------------------------#
 
-    chosen_pelt_color = choice(
-        random.choices(colour_categories, weights=(40, 40, 40, 40, 40, 40, 10, 10, 10, 20, 20), k=1)[0]
-    )
+    chosen_pelt_color = choice((pride_colours * 2) + random.choices(colour_categories, k=1)[0])
 
     # ------------------------------------------------------------------------------------------------------------#
     #   PELT LENGTH
