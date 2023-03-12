c170f7da47f7f9c932addf377234e6d1e3aca55a
MaryHawkes <42361363+MaryHawkes@users.noreply.github.com>
2023-03-12 15:20:06 +1300

    Random patrols first working

--- scripts/screens/patrol_screens.py
+++ scripts/screens/patrol_screens.py
@@ -503,27 +503,32 @@ class PatrolScreen(Screens):
         text = text.replace('c_n', str(game.clan.name) + 'Clan')
 
         # Prey lists for forest random prey patrols
-        fst_tinyprey_singlular = ['shrew', 'robin', 'vole', 'dormouse', 'blackbird',
+        fst_tinyprey_singlular_list = ['shrew', 'robin', 'vole', 'dormouse', 'blackbird',
                                   'wood mouse', 'lizard', 'tiny grass snake', 'finch', 'sparrow',
                                   'small bird', 'young rat', 'young hedgehog', 'big beetle', 'woodrat',
                                   'white-footed mouse', 'golden mouse', 'young squirrel', 'chipmunk', ]
+        fst_tinyprey_singlular = choice(fst_tinyprey_singlular_list)
         text = text.replace('f_tp_s', str(fst_tinyprey_singlular))
 
-        fst_tinyprey_plural = ['mice', 'mice', 'mice', 'shrews', 'robins', 'voles', 'mice', 'blackbirds',
+        fst_tinyprey_plural_list = ['mice', 'mice', 'mice', 'shrews', 'robins', 'voles', 'mice', 'blackbirds',
                                'mice', 'mice', 'lizards', 'small birds', 'small birds', 'sparrows',
                                'sleepy dormice', 'chipmunks', 'woodrats', ]
+        fst_tinyprey_plural = choice(fst_tinyprey_plural_list)
         text = text.replace('f_tp_p', str(fst_tinyprey_plural))
 
-        fst_midprey_singlular = ['plump shrew', 'woodpecker', 'mole', 'fat dormouse', 'blackbird',
+        fst_midprey_singlular_list = ['plump shrew', 'woodpecker', 'mole', 'fat dormouse', 'blackbird',
                                  'field vole', 'big lizard', 'grass snake', 'half-grown rabbit', 'hedgehog',
                                  'red squirrel', 'grey squirrel', 'rat', 'flying squirrel', 'kingfisher', ]
+        fst_midprey_singlular = choice(fst_midprey_singlular_list)
         text = text.replace('f_mp_s', str(fst_midprey_singlular))
 
-        fst_midprey_plural = ['plump shrews', 'woodpeckers', 'moles', 'blackbirds',
+        fst_midprey_plural_list = ['plump shrews', 'woodpeckers', 'moles', 'blackbirds',
                               'field voles', 'big lizards', 'grass snakes', 'half-grown rabbits', 'hedgehogs',
                               'red squirrels', 'grey squirrels', 'rats', ]
+        fst_midprey_plural = choice(fst_midprey_plural_list)
         text = text.replace('f_mp_p', str(fst_midprey_plural))
 
+        # Sings for medicine cat omens
         sign_list = ['strangely-patterned stone', 'sharp stick', 'prey bone', 'cloud shaped like a cat',
                      'tuft of red fur', 'red feather', 'brown feather', 'black feather', 'white feather',
                      'star-shaped leaf',
