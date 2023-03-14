1646293785db034cd9fca07805a1cd8c9357ed59
Philipp Micke <archanyhm@gmail.com>
2023-03-14 13:06:52 +0100

    Add NoopWriter to avoid crashing
    This is particularly important since sys.stdout and sys.stderr
    are None on Windows GUI applications

--- scripts/stream_duplexer.py
+++ scripts/stream_duplexer.py
@@ -1,7 +1,16 @@
+from scripts.noop_writer import NoopWriter
+
+
 class BufferedStreamDuplexer:
-    def __init__(self, first_stream, second_stream):
+    def __init__(self, first_stream, second_stream, noop_writer_fallback=True):
         self.firstStream = first_stream
         self.secondStream = second_stream
+
+        if noop_writer_fallback:
+            if self.firstStream is None:
+                self.firstStream = NoopWriter()
+            if self.secondStream is None:
+                self.secondStream = NoopWriter()
 
     def write(self, data):
         self.firstStream.write(data)
@@ -13,9 +22,15 @@ class BufferedStreamDuplexer:
 
 
 class UnbufferedStreamDuplexer:
-    def __init__(self, first_stream, second_stream):
+    def __init__(self, first_stream, second_stream, noop_writer_fallback=True):
         self.firstStream = first_stream
         self.secondStream = second_stream
+
+        if noop_writer_fallback:
+            if self.firstStream is None:
+                self.firstStream = NoopWriter()
+            if self.secondStream is None:
+                self.secondStream = NoopWriter()
 
     def write(self, data):
         self.firstStream.write(data)
