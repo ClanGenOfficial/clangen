e501db19fb889505f7476d0ce13f7c28d2065801
Maxxicloudd <122584598+Maxxicloudd@users.noreply.github.com>
2023-03-16 21:28:56 -0500

    Fixed the history tab so it doesn't crash the game
    changed all instances of game.settings['favorite sub tab'] to game.switches['favorite_sub_tab'] so that it references the correct dictionary and key

--- scripts/screens/cat_screens.py
+++ scripts/screens/cat_screens.py
@@ -291,10 +291,10 @@ class ProfileScreen(Screens):
                 self.toggle_dangerous_tab()
             elif event.ui_element == self.backstory_tab_button:
                 if self.open_sub_tab is None:
-                    if game.settings['favorite sub tab'] is None:
+                    if game.switches['favorite_sub_tab'] is None:
                         self.open_sub_tab = 'life events'
                     else:
-                        self.open_sub_tab = game.settings['favorite sub tab']
+                        self.open_sub_tab = game.switches['favorite_sub_tab']
 
                 self.toggle_history_tab()
             elif event.ui_element == self.conditions_tab_button:
