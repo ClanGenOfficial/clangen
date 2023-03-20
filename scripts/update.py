4b66533542eeae0e26d664297ab0c9b1b575895a
Philipp Micke <archanyhm@gmail.com>
2023-03-20 11:02:35 +0100

    Try to force windows to detach the process

--- scripts/update.py
+++ scripts/update.py
@@ -166,7 +166,7 @@ def self_update(release_channel='development-test', progress_bar: UIUpdateProgre
         print("Powershell will now be used to update Clangen.")
         print("A console window will open and close automatically. Please do not be alarmed.")
 
-        os.execv('./clangen_update_script.bat', [os.getcwd()])
+        subprocess.Popen(["./clangen_update_script.bat", os.getcwd()], cwd=path, creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_BREAKAWAY_FROM_JOB)
         quit()
 
     elif platform.system() == 'Darwin':
