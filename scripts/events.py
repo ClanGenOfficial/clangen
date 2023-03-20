b520346a12ed189b42bdc944f50b261da83a8c4a
cat <101748822+capillarys@users.noreply.github.com>
2023-03-20 12:26:08 +0000

    back to the original
    sorry change didn't work

--- scripts/events.py
+++ scripts/events.py
@@ -1182,7 +1182,7 @@ class Events():
 
         # getting the random honor if it's needed
         random_honor = None
-        if promoted_to == ['warrior', 'mediator', 'med']:
+        if promoted_to == 'warrior':
             resource_dir = "resources/dicts/events/ceremonies/"
             with open(f"{resource_dir}ceremony_traits.json",
                       encoding="ascii") as read_file:
