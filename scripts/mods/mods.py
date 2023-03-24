fafdc73c45e6226e7f6b047fbe934d7e828b0121
Luna <imlvnaa@gmail.com>
2023-03-24 12:55:34 -0400

    migrate to a class wip

--- scripts/mods/mods.py
+++ scripts/mods/mods.py
@@ -18,7 +18,7 @@ def get_all_mods():
     """
     mods = []
     for mod in os.listdir(get_mods_dir()):
-        if os.path.isdir(os.path.join(get_mods_dir(), mod)):
+        if os.path.isdir(os.path.join(get_mods_dir(), mod)) and mod != ".modsettings.json":
             mods.append(mod)
     return mods
 
@@ -36,9 +36,28 @@ def get_real_mods():
 
         dircontents = os.listdir(os.path.join(get_mods_dir(), mod))
 
+
+        # God bless anyone who has to read this
+        overwritten_scripts = {}
         for folder in required_folders:
             if folder in dircontents:
-                modvalid = True
+                _dircontents = os.listdir(os.path.join(get_mods_dir(), mod, folder))
+                if _dircontents:
+                    modvalid = True
+                    if folder == "scripts":
+                        _overwritten_scripts = []
+                        for file in _dircontents:
+                            if os.path.isdir(os.path.join(get_mods_dir(), mod, folder, file)):
+                                for _file in os.listdir(os.path.join(get_mods_dir(), mod, folder, file)):
+                                    if _file.endswith(".py"):
+                                        _overwritten_scripts.append(os.path.join(file, _file))
+                            if file.endswith(".py"):
+                                _overwritten_scripts.append(file)
+                        overwritten_scripts[mod] = _overwritten_scripts
+
+        for mod, scripts in overwritten_scripts.items():
+            print(f"Mod {mod} overwrites the following scripts: {', '.join(scripts)}")
+
 
         if not modvalid:
             print(f"Mod {mod} has no overriden resources, scripts, or sprites. It will be ignored.")
@@ -49,12 +68,78 @@ def get_real_mods():
 
 
 
+class modList():
+    """
+    TODO: docs
+    """
+    def __init__(self):
+        self._mods = # TODO()
+        self.mods = self.sort(self._mods, should_reread=True)
 
-def get_mods():
-    """
-    Returns a list of all mods in the mods folder
-    This takes priority into account
-    This takes toggling into account
-    """
-    # TODO: priority
-    return get_real_mods()
+    def sort(self, mods_to_sort=[], should_reread=False, should_rewrite=False):
+        """
+        reads <mods-dir>/.modsettings.json
+        each line is a mod
+        the first mod is the highest priority
+        if a mod is not in the file, it is assumed to be the lowest priority, and is appended to the end # pylint: disable=line-too-long
+
+        If should_rewrite is True, it will *always* rewrite the file
+        if false, it will only rewrite if it needs to
+        """
+        if mods_to_sort == []:
+            return mods_to_sort
+        if not os.path.exists(os.path.join(get_mods_dir(), ".modsettings.json")):
+            return mods_to_sort.sort() # a-z
+
+        sorted_mods = []
+        if should_reread:
+            with open(os.path.join(get_mods_dir(), ".modsettings.json"), "r", encoding='ascii') as file: # pylint: disable=line-too-long
+                prefs = file.readlines()
+        else:
+            prefs = self.mods # read the already sorted mods list, and sort it again
+        for mod in prefs:
+            if mod in mods_to_sort:
+                sorted_mods.append(mod)
+            else:
+                print(f"Mod {mod} is not installed. It will be removed")
+                should_rewrite = True
+
+        for mod in mods_to_sort:
+            if mod not in sorted_mods:
+                sorted_mods.append(mod)
+                should_rewrite = True
+
+        if should_rewrite:
+            self.write_modsettings(sorted_mods)
+        return sorted_mods
+
+    def write_modsettings(self, mods):
+        """
+        THIS SHOULD ALWAYS BE CALLED AFTER SORTING
+        Preferrably, use self.sort(should_rewrite=True)
+        writes <mods-dir>/.modsettings.json
+        """
+        with open(os.path.join(get_mods_dir(), ".modsettings.json"), "w", encoding='ascii') as file:
+            file.writelines(mods)
+
+    def add(self, mod):
+        """
+        Adds a mod to the list
+        """
+        if mod in self.mods:
+            return
+        self.mods.append(mod)
+        self._mods.append(mod)
+        self.sort(should_rewrite=True)
+
+    def remove(self, mod):
+        """
+        Removes a mod from the list
+        """
+        if mod not in self.mods:
+            return
+        self.mods.remove(mod)
+        self._mods.remove(mod)
+        self.sort(should_rewrite=True)
+
+    
