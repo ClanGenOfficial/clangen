004065afa6baffc4a3ec02607345d86d8a22143a
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2023-03-27 11:13:02 -0500

    yeah

--- tests/test_thoughts.py
+++ tests/test_thoughts.py
@@ -4,7 +4,7 @@ import os
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
 
-from scripts.cat.thoughts import resource_directory, get_med_thoughts, get_family_thoughts
+from scripts.cat.thoughts import load_thoughts
 
 try:
     import ujson
@@ -15,27 +15,7 @@ from scripts.cat.cats import Cat
 
 class TestsGetStatusThought(unittest.TestCase):
 
-    def load_resources(self, status):
-        base_path = f"resources/dicts/thoughts/"
-        status = "warrior"
-        life_dir = "alive"
-        spec_dir = ""
-
-        THOUGHTS = []
-        with open(f"{base_path}{life_dir}{spec_dir}/{status}.json", 'r') as read_file:
-            THOUGHTS = ujson.loads(read_file.read())
-        GENTHOUGHTS = []
-        with open(f"{base_path}{life_dir}{spec_dir}/general.json", 'r') as read_file:
-            GENTHOUGHTS = ujson.loads(read_file.read())
-
-        return [THOUGHTS,
-                GENTHOUGHTS]
-
-    def test_medicine_thought(self, status):
-        status = "medicine cat"
-        [THOUGHTS,
-         GENTHOUGHTS] = self.load_resources()
-
+    def test_medicine_thought(self):
         # given
         medicine = Cat()
         warrior = Cat()
@@ -43,34 +23,39 @@ class TestsGetStatusThought(unittest.TestCase):
         warrior.status = "warrior"
         medicine.trait = "bold"
 
+        # load thoughts
+        thoughts = load_thoughts(medicine, warrior, "expanded")
+
+        # when
+        function_thoughts = thoughts
+
+
+
 class TestFamilyThoughts(unittest.TestCase):
-    def load_resources(self):
-        base_path = f"resources/dicts/thoughts/"
-        status = "warrior"
-        life_dir = "alive"
-        spec_dir = ""
-
-        THOUGHTS = []
-        with open(f"{base_path}{life_dir}{spec_dir}/{status}.json", 'r') as read_file:
-            THOUGHTS = ujson.loads(read_file.read())
-        GENTHOUGHTS = []
-        with open(f"{base_path}{life_dir}{spec_dir}/general.json", 'r') as read_file:
-            GENTHOUGHTS = ujson.loads(read_file.read())
-
-        return [THOUGHTS,
-                GENTHOUGHTS]
 
     def test_family_thought_young_children(self):
         # given
-        FAMILY = self.load_resources()
         parent = Cat(moons=40)
         kit = Cat(parent1=parent.ID, moons=4)
         parent.children.append(kit.ID)
 
+        # when
+        function_thoughts1 = load_thoughts(parent, kit)
+        function_thoughts2 = load_thoughts(kit, parent)
+
+        # then
+        '''
+        self.assertTrue(all(t in own_collection_thoughts for t in function_thoughts1))
+        self.assertFalse(all(t in not_collection_thoughts for t in function_thoughts1))
+        self.assertEqual(function_thoughts2,[])
+        '''
     
     def test_family_thought_unrelated(self):
         # given
         cat1 = Cat(moons=40)
         cat2 = Cat(moons=40)
 
+        # when
+
+        # then
 
