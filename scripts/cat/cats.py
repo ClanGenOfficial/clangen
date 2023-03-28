52bf4113544ba7a8812cf12a494fcefa526c23f8
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2023-03-28 14:17:16 -0500

    fix: remove redundant traits

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -43,10 +43,10 @@ class Cat():
     dead_cats = []
     used_screen = screen
     traits = [
-        'adventurous', 'altruistic', 'ambitious', 'bloodthirsty', 'bold',
+        'adventurous', 'ambitious', 'bloodthirsty', 'bold',
         'calm', 'careful', 'charismatic', 'childish', 'cold', 'compassionate',
-        'confident', 'daring', 'empathetic', 'faithful', 'fierce', 'insecure',
-        'lonesome', 'loving', 'loyal', 'nervous', 'patient', 'playful',
+        'confident', 'daring', 'faithful', 'fierce', 'insecure',
+        'lonesome', 'loving', 'loyal', 'nervous', 'playful',
         'responsible', 'righteous', 'shameless', 'sneaky', 'strange', 'strict',
         'thoughtful', 'troublesome', 'vengeful', 'wise'
     ]
@@ -58,8 +58,7 @@ class Cat():
     personality_groups = {
         'Outgoing': ['adventurous', 'bold', 'charismatic', 'childish', 'confident', 'daring',
                      'playful', 'righteous', 'attention-seeker', 'bouncy', 'charming', 'noisy'],
-        'Benevolent': ['altruistic', 'compassionate', 'empathetic', 'faithful', 'loving',
-                       'patient', 'responsible', 'thoughtful', 'wise', 'inquisitive',
+        'Benevolent': ['faithful', 'loving', 'responsible', 'thoughtful', 'wise', 'inquisitive',
                        'polite', 'sweet'],
         'Abrasive': ['ambitious', 'bloodthirsty', 'cold', 'fierce', 'shameless', 'strict',
                      'troublesome', 'vengeful', 'bossy', 'bullying', 'impulsive'],
