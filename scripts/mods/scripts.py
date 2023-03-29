e96340f4ae3f608b0e9289ad0820f4c5ee43669d
Luna <imlvnaa@gmail.com>
2023-03-29 10:17:58 -0400

    comment

--- scripts/mods/scripts.py
+++ scripts/mods/scripts.py
@@ -3,6 +3,14 @@
 USED VIA:
 <top of script, below module comments, above imports>
 
+from scripts.mods.scripts import check_and_run
+_ = check_and_run(__file__)
+if _ is not '':
+    import importlib
+    return importlib.import_module(_.replace(os.sep, ".").replace(".py", ""))
+else:
+    del check_and_run
+    del _
 """
 
 
@@ -11,9 +19,9 @@ import os
 import importlib
 
 
-def check_and_run(scriptname: str):
+def check_and_run(scriptname: str) -> str:
     for mod in modlist.get_mods():
         if os.path.exists(os.path.join(os.getcwd(), "mods", mod, scriptname)):
-            _ = os.path.join(os.getcwd(), "mods", mod, scriptname)
-            return True, importlib.import_module(_)
-    return False, None
+            _ = os.path.join("mods", mod, scriptname)
+            return _
+    return ''
