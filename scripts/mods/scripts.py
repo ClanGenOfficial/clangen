62d645e518308248fcd42924b6926cd09a35722e
Luna <imlvnaa@gmail.com>
2023-03-29 09:13:28 -0400

    pusl

---
+++ scripts/mods/scripts.py
@@ -0,0 +1,11 @@
+from .mods import modlist
+import os
+import importlib
+
+
+def check_and_run(scriptname: str):
+    for mod in modlist.get_mods():
+        if os.path.exists(os.path.join(os.getcwd(), "mods", mod, scriptname)):
+            _ = os.path.join(os.getcwd(), "mods", mod, scriptname)
+            return True, importlib.import_module(_)
+    return False, None
