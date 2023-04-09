e3af43f019257f0d6b40518fac5c319a2f6e0a66
Philipp Micke <archanyhm@gmail.com>
2023-04-09 03:37:31 +0200

    Test self-update!

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ else:
 if get_version_info().is_source_build or get_version_info().is_dev():
     dev_watermark = pygame_gui.elements.UILabel(
         scale(pygame.Rect((1050, 1321), (600, 100))),
-        "Dev Build:",
+        "Hi Scribble!:",
         object_id="#dev_watermark"
     )
 
