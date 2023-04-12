16710da271e5f46ae1f2c592fc5ac709dfda95f6
scribblecrumb <abspence2199@gmail.com>
2023-04-12 11:22:43 -0500

    updating patrol chances

--- scripts/events.py
+++ scripts/events.py
@@ -174,7 +174,7 @@ class Events():
                         f"lives are shared in stories around the circle of mourners as those that were closest to them " \
                         f"take them to their final resting place."
 
-                if len(ghost_names) > 2:
+                if len(ghost_names) > 2 and game.clan.game_mode != 'classic':
                     alive_cats = list(
                         filter(
                             lambda kitty: (kitty.status != "leader" and not kitty.dead and
