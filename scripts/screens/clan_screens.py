1e3289447e275e099db4879c6672a86669fc1e67
Klara <79152963+drKhaki@users.noreply.github.com>
2023-04-19 16:07:35 +0200

    smol tweak

--- scripts/screens/clan_screens.py
+++ scripts/screens/clan_screens.py
@@ -1388,7 +1388,7 @@ class AllegiancesScreen(Screens):
     def screen_switches(self):
         # Heading
         self.heading = pygame_gui.elements.UITextBox(f'{game.clan.name}Clan Allegiances',
-                                                     scale(pygame.Rect((400, 230), (800, 80))),
+                                                     scale(pygame.Rect((390, 230), (800, 80))),
                                                      object_id=get_text_box_theme("#text_box_34_horizcenter")
                                                      , manager=MANAGER)
 
