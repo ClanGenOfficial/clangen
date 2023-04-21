c00792d8212be94e6dfd163ed30e103834702b7b
Luna R <imlvnaa@gmail.com>
2023-04-21 13:13:19 -0400

    fix newline and quote encasing

--- build.py
+++ build.py
@@ -10,23 +10,19 @@ if directory:
 print('Creating temporary files...')
 os.makedirs('tmp', exist_ok=True)
 with open('tmp/version.ini', 'w') as f:
-    f.write(f"""
-    [DEFAULT]
-    version_number={os.environ.get('VERSION_NUMBER', '0.0.0')}
-    release_channel={os.environ.get('RELEASE_CHANNEL', 'development')}
-    upstream={os.environ.get('UPSTREAM', 'Thlumyn/clangen')}
-    """)
+    f.write(f"""[DEFAULT]
+version_number={os.environ.get('VERSION_NUMBER', '0.0.0')}
+release_channel={os.environ.get('RELEASE_CHANNEL', 'development')}
+upstream={os.environ.get('UPSTREAM', 'Thlumyn/clangen')}""")
 
 isDev = os.environ.get('RELEASE_CHANNEL', 'development') == 'development'
 
 if platform.system() == 'Windows':
     with open('tmp/datadir.bat', 'w') as f:
-        f.write(fr"""
-start "%LocalAppData%\ClanGen\ClanGen{'Beta' if isDev else ''}"
-""")
+        f.write(fr"""start %LocalAppData%\ClanGen\ClanGen{'Beta' if isDev else ''}""")
 elif platform.system() == 'Linux':
     with open('tmp/datadir.sh', 'w') as f:
-        f.write(f"""#!/bin/sh
+        f.write(f"""#!/bin/shs
 if [ -z "$XDG_DATA_HOME" ]; then
     xdg-open "$XDG_DATA_HOME/ClanGen/ClanGen{'Beta' if isDev else ''}"
 else
