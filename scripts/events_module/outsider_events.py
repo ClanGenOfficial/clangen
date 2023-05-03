3bfc5467b6e4a0121afebad7b3ceb646a25fe7f7
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2023-05-03 09:21:04 -0500

    content: outsider event module

---
+++ scripts/events_module/outsider_events.py
@@ -0,0 +1,44 @@
+import random
+
+from scripts.cat.cats import Cat
+from scripts.events_module.generate_events import GenerateEvents
+from scripts.game_structure.game_essentials import game
+from scripts.event_class import Single_Event
+
+
+# ---------------------------------------------------------------------------- #
+#                               New Cat Event Class                              #
+# ---------------------------------------------------------------------------- #
+
+class OutsiderEvents:
+    """All events with a connection to outsiders."""
+
+    def __init__(self) -> None:
+        self.event_sums = 0
+        self.had_one_event = False
+        self.generate_events = GenerateEvents()
+        pass
+
+    def killing_outsiders(self, cat: Cat):
+        # killing outside cats
+        if cat.outside:
+            if random.getrandbits(6) == 1 and not cat.dead:
+                cat.dead = True
+                if cat.exiled:
+                    text = f'Rumors reach your Clan that the exiled {cat.name} has died recently.'
+                elif cat.status in ['kittypet', 'loner', 'rogue', 'former Clancat']:
+                    text = f'Rumors reach your Clan that the {cat.status} ' \
+                           f'{cat.name} has died recently.'
+                else:
+                    cat.outside = False
+                    text = f"Will they reach StarClan, even so far away? {cat.name} isn't sure, " \
+                           f"but as they drift away, they hope to see " \
+                           f"familiar starry fur on the other side."
+                game.cur_events_list.append(
+                    Single_Event(text, "birth_death", cat.ID))
+                
+    def lost_cat_become_outsider(self, cat: Cat):
+        """ 
+        this will be for lost cats becoming kittypets/loners/etc
+        TODO: need to make a unique backstory for these cats so they still have thoughts related to their clan
+        """
