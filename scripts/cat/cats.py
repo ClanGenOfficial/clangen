08d1be6918acbfb67fe1b76c754c01d2dc7c54d4
keyraven <118307471+keyraven@users.noreply.github.com>
2023-05-16 16:39:39 -0600

    Fix pylint 2

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -1559,7 +1559,7 @@ class Cat():
                                 possible_skill = self.skill_groups.get(x)
                                 self.skill = choice(possible_skill)
                                 skill_influence = self.skill
-                                History.add_skill_mentor_influence(self, mentor, skill_influence)
+                                History.add_skill_mentor_influence(self, skill_influence)
                                 return
 
                     all_skills = []
@@ -1567,7 +1567,7 @@ class Cat():
                         all_skills.extend(self.skill_groups[x])
                     self.skill = choice(all_skills)
 
-                    History.add_skill_mentor_influence(self, mentor, skill_influence)
+                    History.add_skill_mentor_influence(self, skill_influence)
 
 
             # assign new skill to elder
