4b76aa3263ad58d3836193de70a5b20aed0abaf4
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-06-04 13:07:18 -0500

    add menu art
    from Meltedcandles#7100
    bumblebeez#6804
    vividchaos#8963
    GooseEatsHolyWater#3091
    Mushie Marsh!#3110

--- scripts/screens/organizational_screens.py
+++ scripts/screens/organizational_screens.py
@@ -53,14 +53,8 @@ class StartScreen(Screens):
     def __init__(self, name=None):
         super().__init__(name)
         self.warning_label = None
-        bg = random.randint(1,3)
-        if bg == 1:
-            self.bg = pygame.image.load("resources/images/menu3.png").convert()
-        elif bg == 2:
-            self.bg = pygame.image.load("resources/images/menu2.png").convert()
-        else:
-            self.bg = pygame.image.load("resources/images/menu.png").convert()
-
+        bg = random.randint(1,8)
+        self.bg = pygame.image.load("resources/images/menu" + str(bg) + ".png").convert()
         self.bg = pygame.transform.scale(self.bg, (screen_x, screen_y))
         self.social_buttons = {}
 
