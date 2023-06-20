fb78b750feee1215faee01129ae258a4d1f3587c
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-06-20 15:21:40 -0500

    ADD TALKS

--- scripts/screens/cat_screens.py
+++ scripts/screens/cat_screens.py
@@ -22,7 +22,7 @@ from scripts.game_structure import image_cache
 import pygame_gui
 from re import sub
 from scripts.game_structure.image_button import UIImageButton, UITextBoxTweaked
-from scripts.game_structure.game_essentials import game, screen_x, screen_y, MANAGER
+from scripts.game_structure.game_essentials import game, screen_x, screen_y, MANAGER, screen
 from scripts.cat.names import names, Name
 from scripts.clan_resources.freshkill import FRESHKILL_ACTIVE
 
@@ -3232,16 +3232,22 @@ class TalkScreen(Screens):
 
 
     def screen_switches(self):
+        self.update_camp_bg()
         self.hide_menu_buttons()
         self.text_index = 0
         self.frame_index = 0
         self.the_cat = Cat.all_cats.get(game.switches['cat'])
         self.profile_elements = {}
-
+        self.clan_name_bg = pygame_gui.elements.UIImage(
+            scale(pygame.Rect((450, 875), (380, 70))),
+            pygame.transform.scale(
+                image_cache.load_image(
+                    "resources/images/clan_name_bg.png").convert_alpha(),
+                (500, 870)),
+            manager=MANAGER)
         self.profile_elements["cat_name"] = pygame_gui.elements.UITextBox(str(self.the_cat.name),
                                                                           scale(pygame.Rect((500, 870), (-1, 80))),
-                                                                          object_id=get_text_box_theme(
-                                                                              "#text_box_40_horizcenter"),
+                                                                          object_id="#text_box_34_horizcenter_light",
                                                                           manager=MANAGER)
         self.texts = self.get_possible_text(self.the_cat)
         self.text_frames = [[text[:i+1] for i in range(len(text))] for text in self.texts]
@@ -3256,7 +3262,7 @@ class TalkScreen(Screens):
         self.scroll_container = pygame_gui.elements.UIScrollingContainer(scale(pygame.Rect((500, 970), (900, 300))))
         self.text = pygame_gui.elements.UITextBox("", 
                                                   scale(pygame.Rect((0, 0), (900, -100))),
-                                                  object_id=get_text_box_theme("#text_box_30_horizleft"),
+                                                  object_id="#text_box_30_horizleft",
                                                   container=self.scroll_container, manager=MANAGER)
         self.profile_elements["cat_image"] = pygame_gui.elements.UIImage(scale(pygame.Rect((70, 900), (400, 400))),
 
@@ -3275,12 +3281,55 @@ class TalkScreen(Screens):
         self.profile_elements["cat_image"].kill()
         self.profile_elements["cat_name"].kill()
         del self.profile_elements
-
+        self.clan_name_bg.kill()
+        del self.clan_name_bg
         self.talk_box.kill()
         del self.talk_box
 
+    def update_camp_bg(self):
+        light_dark = "light"
+        if game.settings["dark mode"]:
+            light_dark = "dark"
+
+        camp_bg_base_dir = 'resources/images/camp_bg/'
+        leaves = ["newleaf", "greenleaf", "leafbare", "leaffall"]
+        camp_nr = game.clan.camp_bg
+
+        if camp_nr is None:
+            camp_nr = 'camp1'
+            game.clan.camp_bg = camp_nr
+
+        available_biome = ['Forest', 'Mountainous', 'Plains', 'Beach']
+        biome = game.clan.biome
+        if biome not in available_biome:
+            biome = available_biome[0]
+            game.clan.biome = biome
+        biome = biome.lower()
+
+        all_backgrounds = []
+        for leaf in leaves:
+            platform_dir = f'{camp_bg_base_dir}/{biome}/{leaf}_{camp_nr}_{light_dark}.png'
+            all_backgrounds.append(platform_dir)
+
+        self.newleaf_bg = pygame.transform.scale(
+            pygame.image.load(all_backgrounds[0]).convert(), (screen_x, screen_y))
+        self.greenleaf_bg = pygame.transform.scale(
+            pygame.image.load(all_backgrounds[1]).convert(), (screen_x, screen_y))
+        self.leafbare_bg = pygame.transform.scale(
+            pygame.image.load(all_backgrounds[2]).convert(), (screen_x, screen_y))
+        self.leaffall_bg = pygame.transform.scale(
+            pygame.image.load(all_backgrounds[3]).convert(), (screen_x, screen_y))
+    
     def on_use(self):
-            
+        if game.settings['backgrounds']:
+            if game.clan.current_season == 'Newleaf':
+                screen.blit(self.newleaf_bg, (0, 0))
+            elif game.clan.current_season == 'Greenleaf':
+                screen.blit(self.greenleaf_bg, (0, 0))
+            elif game.clan.current_season == 'Leaf-bare':
+                screen.blit(self.leafbare_bg, (0, 0))
+            elif game.clan.current_season == 'Leaf-fall':
+                screen.blit(self.leaffall_bg, (0, 0))    
         now = pygame.time.get_ticks()
         if now >= self.next_frame_time and self.frame_index < len(self.text_frames[self.text_index]) - 1:
             self.frame_index += 1
@@ -3344,8 +3393,11 @@ class TalkScreen(Screens):
                     continue
                 if cat.relationships[game.clan.your_cat.ID].trust < 30 and 'trust' in talk[0]:
                     continue
-            
-            texts_list.append(talk[1])
+                texts_list.append(talk[1])
+            else:
+                if any(i in ['hate', 'crush', 'romantic_like', 'platonic_like', 'jealousy', 'dislike', 'comfort', 'respect', 'trust'] for i in talk[0]):
+                    continue
+                texts_list.append(talk[1])
         if not texts_list:
             resource_dir = "resources/dicts/lifegen_talk/"
             possible_texts = None
@@ -3354,10 +3406,10 @@ class TalkScreen(Screens):
             texts_list.append(possible_texts['general'][1])
         
         text = choice(texts_list)
-        text = [t.replace("c_n", game.clan.name) for t in text]
-        text = [t.replace("y_c", str(game.clan.your_cat.name)) for t in text]
+        text = [t1.replace("c_n", game.clan.name) for t1 in text]
+        text = [t1.replace("y_c", str(game.clan.your_cat.name)) for t1 in text]
 
-        return choice(texts_list)
+        return text
         
         
         
