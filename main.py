edf0be44edcd2faafc0fbd91e0b31f405d3c345c
keyraven <118307471+keyraven@users.noreply.github.com>
2023-07-28 19:10:38 -0600

    Use pygame.time.wait for loading animation.

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def loading_animation():
         rotated = pygame.transform.rotate(image, angle)
         screen.blit(rotated, (x - rotated.get_width() / 2 , y - rotated.get_height() / 2))
         angle += 1
-        pygame.time.delay(10)
+        pygame.time.wait(10)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
