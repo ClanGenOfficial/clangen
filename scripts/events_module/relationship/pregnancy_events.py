e6b666045fb642ff5b4d7298ee6fbd1efcaa4b37
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-08-18 15:20:31 -0500

    Update pregnancy_events.py

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -110,7 +110,11 @@ class Pregnancy_Events():
             if not game.settings['single parentage']:
                 return
 
-        chance = self.get_balanced_kit_chance(cat, second_parent, is_affair)
+        chance = Pregnancy_Events.get_balanced_kit_chance(cat, second_parent, is_affair)
+        if 'have kits' in game.switches:
+            if cat.ID == game.clan.your_cat.ID and not game.switches['have kits']:
+                chance -= randint(60,100)
+        chance = max(0,chance)
 
         if not int(random.random() * chance):
             # If you've reached here - congrats, kits!
