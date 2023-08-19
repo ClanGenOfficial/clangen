abeca3ecee8e267c1a98ec418987b674bbd8122e
Acorn <a.fellow.corn@gmail.com>
2023-08-19 11:50:28 +0200

    Revert "Random 2 W_P gen"
    This reverts commit 0d96d827c1787c7b51d4e984f4bc87d9f3f588c5.

--- scripts/utility.py
+++ scripts/utility.py
@@ -1253,15 +1253,16 @@ def generate_sprite(cat, life_state=None, scars_hidden=False, acc_hidden=False, 
 
         # draw white patches
         if cat.pelt.white_patches is not None:
-            for white in cat.pelt.white_patches:
-                    if cat.pelt.white_patches_tint != "none" and cat.pelt.white_patches_tint in sprites.white_patches_tints["tint_colours"]:
-                        white_patch = sprites.sprites['white' + white + cat_sprite].copy()
-                        tint = pygame.Surface((sprites.size, sprites.size)).convert_alpha()
-                        tint.fill(tuple(sprites.white_patches_tints["tint_colours"][cat.pelt.white_patches_tint]))
-                        white_patch.blit(tint, (0, 0), special_flags=pygame.BLEND_RGB_MULT)
-                        new_sprite.blit(white_patch, (0, 0))
-                    else:
-                        new_sprite.blit(sprites.sprites['white' + white + cat_sprite], (0, 0))
+            white_patches = sprites.sprites['white' + cat.pelt.white_patches + cat_sprite].copy()
+
+            # Apply tint to white patches.
+            if cat.pelt.white_patches_tint != "none" and cat.pelt.white_patches_tint in sprites.white_patches_tints[
+                "tint_colours"]:
+                tint = pygame.Surface((sprites.size, sprites.size)).convert_alpha()
+                tint.fill(tuple(sprites.white_patches_tints["tint_colours"][cat.pelt.white_patches_tint]))
+                white_patches.blit(tint, (0, 0), special_flags=pygame.BLEND_RGB_MULT)
+
+            new_sprite.blit(white_patches, (0, 0))
 
         # draw vit & points
 
