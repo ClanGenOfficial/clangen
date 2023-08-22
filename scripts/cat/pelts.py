01f2ce55b5c22feb4bd204b03bfb24186768c703
RAYTRAC3R <samipiplup@gmail.com>
2023-08-21 21:00:22 -0500

    Fixed mod pelts unable to be tortie

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -54,6 +54,7 @@ class Pelt():
                     'BRIE', 'BELOVED']
     tortiebases = ['single', 'tabby', 'bengal', 'marbled', 'ticked', 'smoke', 'rosette', 'speckled', 'mackerel',
                 'classic', 'sokoke', 'agouti', 'singlestripe']
+    
 
     pelt_length = ["short", "medium", "long"]
     eye_colours = ['YELLOW', 'AMBER', 'HAZEL', 'PALEGREEN', 'GREEN', 'BLUE', 'DARKBLUE', 'GREY', 'CYAN', 'EMERALD', 'PALEBLUE', 
@@ -97,6 +98,7 @@ class Pelt():
     torties = ["Tortie", "Calico"]
     for x in sprites.spritemods:
         for sheet in sprites.spritemods[x]["pelts"]:
+            tortiebases.append(sheet)
             exotic.append(sheet)
     pelt_categories = [tabbies, spotted, plain, exotic, torties]
 
