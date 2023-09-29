8f2485172c366f6e549e520bca3c080e7cdd4cf8
Lara <63805048+larkgz@users.noreply.github.com>
2023-09-29 17:17:43 -0400

    refactor: Remove unused function

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -44,23 +44,6 @@ class Sprites():
         """
         self.spritesheets[name] = pygame.image.load(a_file).convert_alpha()
 
-    def find_sprite(self, group_name, x, y):
-        """
-        Find singular sprite from a group.
-
-        Parameters:
-        group_name -- Name of Pygame group to find sprite from.
-        x -- X-offset of the sprite to get. NOT pixel offset, but offset of other sprites.
-        y -- Y-offset of the sprite to get. NOT pixel offset, but offset of other sprites.
-        """
-        # pixels will be calculated automatically, so for x and y, just use 0, 1, 2, 3 etc.
-        new_sprite = pygame.Surface((self.size, self.size),
-                                    pygame.HWSURFACE | pygame.SRCALPHA)
-        new_sprite.blit(self.groups[group_name], (0, 0),
-                        (x * self.size, y * self.size, (x + 1) * self.size,
-                         (y + 1) * self.size))
-        return new_sprite
-
     def make_group(self,
                    spritesheet,
                    pos,
