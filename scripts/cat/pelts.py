bcd2df124e2692bc28835773f5693d9851a0658a
Ryos00 <112637360+Ryos00@users.noreply.github.com>
2023-10-14 13:32:46 -0500

    content: masked tabby

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -24,6 +24,7 @@ class Pelt():
         'Sokoke': 'sokoke',
         'Agouti': 'agouti',
         'Singlestripe': 'singlestripe',
+        'Masked': 'masked',
         'Tortie': None,
         'Calico': None,
     }
@@ -50,7 +51,7 @@ class Pelt():
                     'ROBIN', 'BRINDLE', 'PAIGE', 'ROSETAIL', 'SAFI', 'SMUDGED', 'DAPPLENIGHT', 'STREAK', 'MASK', 'CHEST', 'ARMTAIL', 'SMOKE', 'GRUMPYFACE',
                     'BRIE', 'BELOVED', 'BODY', 'SHILOH']
     tortiebases = ['single', 'tabby', 'bengal', 'marbled', 'ticked', 'smoke', 'rosette', 'speckled', 'mackerel',
-                'classic', 'sokoke', 'agouti', 'singlestripe']
+                'classic', 'sokoke', 'agouti', 'singlestripe', 'masked']
 
     pelt_length = ["short", "medium", "long"]
     eye_colours = ['YELLOW', 'AMBER', 'HAZEL', 'PALEGREEN', 'GREEN', 'BLUE', 'DARKBLUE', 'GREY', 'CYAN', 'EMERALD', 'PALEBLUE', 
@@ -90,7 +91,7 @@ class Pelt():
     tabbies = ["Tabby", "Ticked", "Mackerel", "Classic", "Sokoke", "Agouti"]
     spotted = ["Speckled", "Rosette"]
     plain = ["SingleColour", "TwoColour", "Smoke", "Singlestripe"]
-    exotic = ["Bengal", "Marbled"]
+    exotic = ["Bengal", "Marbled", "Masked"]
     torties = ["Tortie", "Calico"]
     pelt_categories = [tabbies, spotted, plain, exotic, torties]
 
@@ -458,7 +459,7 @@ class Pelt():
             else:
                 add_weight = (0, 0, 0, 0)
 
-            for x in range(0, len(weights)):
+            for x in enumerate(weights):
                 weights[x] += add_weight[x]
 
             # A quick check to make sure all the weights aren't 0
@@ -486,7 +487,7 @@ class Pelt():
             else:
                 add_weight = (0, 0, 0)
 
-            for x in range(0, len(weights)):
+            for x in enumerate(weights):
                 weights[x] += add_weight[x]
 
         # A quick check to make sure all the weights aren't 0
@@ -797,7 +798,7 @@ class Pelt():
             else:
                 add_weights = (0, 0, 0, 0, 0)
 
-            for x in range(0, len(weights)):
+            for x in enumerate(weights):
                 weights[x] += add_weights[x]
 
 
@@ -972,26 +973,27 @@ class Pelt():
             "Agouti": "c_n tabby",
             "Singlestripe": "dorsal-striped c_n",
             "Rosette": "unusually spotted c_n",
-            "Sokoke": "c_n tabby"
+            "Sokoke": "c_n tabby",
+            "Masked": "masked c_n tabby"
         }
 
-        # Start with determining the base color name. 
+        # Start with determining the base color name
         color_name = str(cat.pelt.colour).lower()
         if color_name in renamed_colors:
             color_name = renamed_colors[color_name]
         
-        # Replace "white" with "pale" if the cat is 
+        # Replace "white" with "pale" if the cat is white
         if cat.pelt.name not in ["SingleColour", "TwoColour", "Tortie", "Calico"] and color_name == "white":
             color_name = "pale"
 
-        # Time to descibe the pattern and any additional colors. 
+        # Time to descibe the pattern and any additional colors
         if cat.pelt.name in pattern_des:
             color_name = pattern_des[cat.pelt.name].replace("c_n", color_name)
         elif cat.pelt.name in Pelt.torties:
-            # Calicos and Torties need their own desciptions. 
+            # Calicos and Torties need their own desciptions
             if short:
-                # If using short, don't add describe the colors of calicos and torties. Just call them calico, tortie, or mottled. 
-                # If using short, don't describe the colors of calicos and torties. Just call them calico, tortie, or mottled. 
+                # If using short, don't add describe the colors of calicos and torties. Just call them calico, tortie, or mottled
+                # If using short, don't describe the colors of calicos and torties. Just call them calico, tortie, or mottled
                 if cat.pelt.colour in Pelt.black_colours + Pelt.brown_colours + Pelt.white_colours and \
                     cat.pelt.tortiecolour in Pelt.black_colours + Pelt.brown_colours + Pelt.white_colours:
                     color_name = "mottled"
@@ -1017,13 +1019,13 @@ class Pelt():
 
         if cat.pelt.white_patches:
             if cat.pelt.white_patches == "FULLWHITE":
-                # If the cat is fullwhite, discard all other information. They are just white. 
+                # If the cat is fullwhite, discard all other information. They are just white
                 color_name = "white"
             if cat.pelt.white_patches in Pelt.mostly_white and cat.pelt.name != "Calico":
                 color_name = f"white and {color_name}"
             elif cat.pelt.name != "Calico":
                 color_name = f"{color_name} and white"
-        
+
         if cat.pelt.points:
             color_name = f"{color_name} point"
             if "ginger point" in color_name:
@@ -1040,10 +1042,10 @@ class Pelt():
         else:
             color_name = f"{color_name} cat"
 
-        # Here is the place where we can add some additional details about the cat, for the full non-short one. 
-        # These include notable missing limbs, vitiligo, long-furred-ness, and 3 or more scars. 
+        # Here is the place where we can add some additional details about the cat, for the full non-short one
+        # These include notable missing limbs, vitiligo, long-furred-ness, and 3 or more scars
         if not short:
-            
+
             scar_details = {
                 "NOTAIL": "no tail", 
                 "HALFTAIL": "half a tail", 
