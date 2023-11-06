7ca2fb85f04eadfc7c86e93f0cd7a6cfb321ea06
Chase1218 <149440038+Chase1218@users.noreply.github.com>
2023-11-05 20:10:45 -0500

    Updated Randomizer
    Added exception for Forest Biome so it will choose Lakeside

--- scripts/screens/MakeClanScreen.py
+++ scripts/screens/MakeClanScreen.py
@@ -390,7 +390,10 @@ class MakeClanScreen(Screens):
             if old_biome is not None:
                 possible_biomes.remove(old_biome)
             self.biome_selected = choice(possible_biomes)
-            self.selected_camp_tab = randrange(1, 5)
+            if self.biome_selected == 'Forest':
+                self.selected_camp_tab = randrange (1, 5)
+            else:
+                self.selected_camp_tab = randrange(1, 4)
             self.refresh_selected_camp()
             self.refresh_text_and_buttons()
         elif event.ui_element == self.elements['done_button']:
