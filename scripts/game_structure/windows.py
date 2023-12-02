fc75be01b77c93a95d8bedd2385023bfd51ab5d8
DeadGirlRisen <Scarletchithekira@gmail.com>
2023-12-02 16:07:18 -0600

    Pronoun

--- scripts/game_structure/windows.py
+++ scripts/game_structure/windows.py
@@ -537,6 +537,10 @@ class SpecifyCatGender(UIWindow):
                 if sub(r'[^A-Za-z0-9 ]+', "", self.gender_entry_box.get_text()) != "":
                     self.the_cat.genderalign = sub(
                         r'[^A-Za-z0-9 ]+', "", self.gender_entry_box.get_text())
+                if self.the_cat.genderalign in ["female", "trans female"]:
+                    self.the_cat.pronouns = [self.the_cat.default_pronouns[1].copy()]
+                if self.the_cat.genderalign in ["male", "trans male"]:
+                    self.the_cat.pronouns = [self.the_cat.default_pronouns[2].copy()]
                 if sub(r'[^A-Za-z0-9 ]+', "", self.subject_entry_box.get_text()) != "":
                     self.the_cat.pronouns[0]["subject"] = sub(
                         r'[^A-Za-z0-9 ]+', "", self.subject_entry_box.get_text())
@@ -552,10 +556,6 @@ class SpecifyCatGender(UIWindow):
                 if sub(r'[^A-Za-z0-9 ]+', "", self.self_entry_box.get_text()) != "":
                     self.the_cat.pronouns[0]["self"] = sub(
                         r'[^A-Za-z0-9 ]+', "", self.self_entry_box.get_text())
-                if self.the_cat.genderalign in ["female", "trans female"]:
-                    self.the_cat.pronouns = [self.the_cat.default_pronouns[1].copy()]
-                if self.the_cat.genderalign in ["male", "trans male"]:
-                    self.the_cat.pronouns = [self.the_cat.default_pronouns[2].copy()]
                 self.gender_changed.show()
             elif event.ui_element == self.back_button:
                 game.switches['window_open'] = False
