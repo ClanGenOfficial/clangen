bca4afb13f12d86c26d8eee31e004e4e04466f78
KiraDragoness <121829433+KiraDragoness@users.noreply.github.com>
2024-05-28 00:16:57 +0200

    Update condition_events.py

--- scripts/events_module/condition_events.py
+++ scripts/events_module/condition_events.py
@@ -683,7 +683,7 @@ class Condition_Events():
                 triggered = True
                 event_types.append("birth_death")
                 event = f"{cat.name} died from complications caused by {condition}."
-                if cat.status == "leader" and game.clan.leader_lives > 1:
+                if cat.status == "leader" and game.clan.leader_lives >= 1:
                     event = f"{cat.name} lost a live to {condition}."
                 event_list.append(event)
 
