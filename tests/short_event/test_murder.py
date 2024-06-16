3aca5cf9e0cb032d7a4f9ed85670523f35b21ecb
J Gynn <48025294+j-gynn@users.noreply.github.com>
2024-06-16 13:34:01 +0100

    Improved murder tests

--- tests/short_event/test_murder.py
+++ tests/short_event/test_murder.py
@@ -31,12 +31,94 @@ class TestShortEventMurder(unittest.TestCase):
         game.clan.instructor = Cat(status="warrior", dead=True, dead_for=20)
         game.clan.all_clans = [Clan(name="Test")]
 
-    def testBasicMurder(self):
+    def tearDown(self):
+        game.clan.leader_lives = 9
+
+    def test_basic_murder(self):
+        """Check basic murder is working as intended"""
         victim = create_victim()
         murderer = create_murderer()
 
-        murderer.relationships[victim.ID] = Relationship(murderer, victim, dislike=30, platonic_like=-15)
+        game.config["event_generation"]["debug_ensure_event_id"] = "gen_death_murder_any1"
 
-        Events.handle_murder(Events(), murderer, force_murder=True)
+        # loathing. unadulterated loathing. for their face, their paws, their accessories.
+        murderer.relationships[victim.ID] = Relationship(murderer, victim, dislike=100, platonic_like=-100)
+
+        # override the calculations to force their hand
+        Events.handle_murder(Events(), murderer, debug_force_murder=True)
+
+        murderer_history = murderer.history.murder["is_murderer"][0]
+        victim_history = victim.history.murder["is_victim"][0]
 
         self.assertTrue(victim.dead)
+        self.assertEqual(victim_history["text"],
+                         f"m_c was secretly murdered by {murderer.name}.")
+
+        self.assertEqual(murderer_history["victim"],
+                         victim.ID)
+
+    def test_leader_murder_some_lives(self):
+        """Check leader murder is working as intended."""
+        victim = create_victim()
+        victim.status = "leader"
+        game.clan.leader = victim
+
+        murderer = create_murderer()
+
+        game.config["event_generation"]["debug_ensure_event_id"] = "gen_death_murder_anylead3"  # only takes some lives
+
+        # loathing. unadulterated loathing. for their face, their paws, their accessories.
+        murderer.relationships[victim.ID] = Relationship(murderer, victim, dislike=100, platonic_like=-100)
+
+        # override the calculations to force their hand
+        Events.handle_murder(Events(), murderer, debug_force_murder=True)
+
+        murderer_history = murderer.history.murder["is_murderer"][0]
+        victim_history = victim.history.murder["is_victim"][0]
+
+        self.assertFalse(victim.dead)
+        self.assertNotEqual(game.clan.leader_lives, 9)
+
+        # reset for the next test
+
+        game.clan.leader_lives = 9
+
+    def test_leader_murder_all_lives(self):
+        """Check leader murder is working as intended."""
+        victim = create_victim()
+        victim.status = "leader"
+
+        murderer = create_murderer()
+
+        game.config["event_generation"]["debug_ensure_event_id"] = "gen_death_murder_anylead4"
+
+        # loathing. unadulterated loathing. for their face, their paws, their accessories.
+        murderer.relationships[victim.ID] = Relationship(murderer, victim, dislike=100, platonic_like=-100)
+
+        # override the calculations to force their hand
+        Events.handle_murder(Events(), murderer, debug_force_murder=True)
+
+        murderer_history = murderer.history.murder["is_murderer"][0]
+        victim_history = victim.history.murder["is_victim"][0]
+
+    def test_secret_murder(self):
+        """check that a secret/unrevealed murder is working as intended"""
+        victim = create_victim()
+        murderer = create_murderer()
+
+        game.config["event_generation"]["debug_ensure_event_id"] = "gen_death_murder_any1"
+
+        murderer.relationships[victim.ID] = Relationship(murderer, victim, dislike=30, platonic_like=-15)
+
+        Events.handle_murder(Events(), murderer, debug_force_murder=True)
+
+        murderer_history = murderer.history.murder["is_murderer"][0]
+        victim_history = victim.history.murder["is_victim"][0]
+
+        self.assertTrue(victim.dead)
+        self.assertEqual(victim_history["text"],
+                         f"m_c was secretly murdered by {murderer.name}.")
+
+        self.assertEqual(murderer_history["victim"],
+                         victim.ID)
+        self.assertFalse(murderer_history["revealed"])
