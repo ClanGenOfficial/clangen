bd1fa0cd7a3fa26a2709bf948c16ad369156e7f5
Sumwatt <172867164+Sumwatt0@users.noreply.github.com>
2024-07-29 12:57:37 -0400

    Switch Run Scripts To Use python3
    Switches run scripts from using python to using python3.
    python sometimes points to python2 so using python3 is more explicit and safe.

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 %AppData%\Python\Scripts\poetry install --no-root
-%AppData%\Python\Scripts\poetry run python main.py
+%AppData%\Python\Scripts\poetry run python3 main.py
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 poetry install --no-root
-poetry run python main.py
+poetry run python3 main.py
