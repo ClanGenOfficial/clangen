3582b31d55602e069b3483b674b2cfc966130f83
selkirks <leafylugia@gmail.com>
2024-08-05 19:55:54 -0400

    more

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -157,7 +157,7 @@ class Sprites:
         # Define eye colors
         eye_colors = [
             ['YELLOW', 'AMBER', 'HAZEL', 'PALEGREEN', 'GREEN', 'BLUE', 'DARKBLUE', 'GREY', 'CYAN', 'EMERALD', 'HEATHERBLUE', 'SUNLITICE'],
-            ['COPPER', 'SAGE', 'COBALT', 'PALEBLUE', 'BRONZE', 'SILVER', 'PALEYELLOW', 'GOLD', 'GREENYELLOW', 'FOXGLOVE', 'OLIVE', 'EASTER']
+            ['COPPER', 'SAGE', 'COBALT', 'PALEBLUE', 'BRONZE', 'SILVER', 'PALEYELLOW', 'GOLD', 'GREENYELLOW']
             ]
         beetle_eyes = [    
             ['ROSE', 'ALGAE', 'SEAFOAM', 'LIGHT FLAME', 'CLOUDY', 'RED', 'TURQUOISE', 'SWAMP', 'RAINY', 'AQUAMARINE', 'EARTH', 'PUMPKIN'],
