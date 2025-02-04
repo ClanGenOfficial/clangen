57a3b2a68a66514bf2d99a31687259121807685d
Lara <63805048+larkgz@users.noreply.github.com>
2025-02-04 15:59:43 -0500

    chore: indent for .dump also

--- utils/json_shim.py
+++ utils/json_shim.py
@@ -5,6 +5,8 @@ Shim for orjson to mimic Python default json API.
 # pylint: disable=no-member
 import orjson
 
+import json
+
 def dumps(obj, indent=0) -> str:
     """
     Serialize `obj` to a JSON formatted `str`.
@@ -24,12 +26,18 @@ def loads(s):
     """
     return orjson.loads(s)
 
-def dump(obj, fp):
+def dump(obj, fp, indent=0):
     """
     Serialize `obj` as a JSON formatted stream to `fp`
     (a `.write()`-supporting file-like object).
+
+    If you specify `indent` as a number greater than 0,
+    indent will ALWAYS BE 2 SPACES.
     """
-    return fp.write(orjson.dumps(obj).decode())
+    opt = 0
+    if indent > 0:
+        opt = orjson.OPT_INDENT_2
+    return fp.write(orjson.dumps(obj, option=opt).decode())
 
 def load(fp):
     """
