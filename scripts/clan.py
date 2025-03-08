7f1798f97d15472d40f704e1c76f936e83c0c72f
Rusty <rusty3925@gmail.com>
2025-03-08 08:04:14 -0800

    txt clan loading

--- scripts/clan.py
+++ scripts/clan.py
@@ -634,13 +634,13 @@ class Clan:
             members = sections[4].split(",")
             other_clans = []
         if len(general) == 9:
-            if general[3] is None or general[3] == "None":
+            if general[3] == "None":
                 general[3] = "camp1"
-            elif general[4] is None or general[4] == "None":
+            elif general[4] == "None":
                 general[4] = 0
-            elif general[7] is None or general[7] == "None":
+            elif general[7] == "None":
                 general[7] = "classic"
-            elif general[8] is None or general[8] == "None":
+            elif general[8] == "None":
                 general[8] = 50
             game.clan = Clan(
                 name=general[0],
@@ -655,11 +655,11 @@ class Clan:
             game.clan.post_initialization_functions()
             game.clan.reputation = general[8]
         elif len(general) == 8:
-            if general[3] is None:
+            if general[3] == "None":
                 general[3] = "camp1"
-            elif general[4] is None:
+            elif general[4] == "None":
                 general[4] = 0
-            elif general[7] is None:
+            elif general[7] == "None":
                 general[7] = "classic"
             game.clan = Clan(
                 name=general[0],
@@ -673,9 +673,9 @@ class Clan:
             )
             game.clan.post_initialization_functions()
         elif len(general) == 7:
-            if general[4] is None:
+            if general[4] == "None":
                 general[4] = 0
-            elif general[3] is None:
+            elif general[3] == "None":
                 general[3] = "camp1"
             game.clan = Clan(
                 name=general[0],
