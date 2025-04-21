50d832b214ba6394b51ef85f8b3c37efeaa5e2cc
scribblecrumb <abspence2199@gmail.com>
2025-04-21 09:39:07 -0500

    Social and Standing enums

--- scripts/cat/enums.py
+++ scripts/cat/enums.py
@@ -19,7 +19,15 @@ class CatAgeEnum(StrEnum):
         return self not in (CatAgeEnum.KITTEN, CatAgeEnum.NEWBORN, CatAgeEnum.ADOLESCENT)
 
 
+class CatSocialEnum(StrEnum):
+    CLANCAT = "clancat"
+    ROGUE = "rogue"
+    LONER = "loner"
+    KITTYPET = "kittypet"
+
+
 class CatRankEnum(StrEnum):
+    # clan ranks
     NEWBORN = "newborn"
     KITTEN = "kitten"
     APPRENTICE = "apprentice"
@@ -30,3 +38,39 @@ class CatRankEnum(StrEnum):
     MEDIATOR = "mediator"
     DEPUTY = "deputy"
     LEADER = "leader"
+    ELDER = "elder"
+
+    # outsider ranks
+    LONER = "loner"
+    ROGUE = "rogue"
+    KITTYPET = "kittypet"
+
+    # TODO: look at Flags for these instead??
+    @property
+    def all_apprentice_ranks(self) -> list:
+        return [CatRankEnum.APPRENTICE, CatRankEnum.MEDIATOR_APPRENTICE, CatRankEnum.MEDICINE_APPRENTICE]
+
+    @property
+    def all_clancat_ranks(self) -> list:
+        return [CatRankEnum.NEWBORN,
+                CatRankEnum.KITTEN,
+                CatRankEnum.APPRENTICE,
+                CatRankEnum.MEDICINE_APPRENTICE,
+                CatRankEnum.MEDIATOR_APPRENTICE,
+                CatRankEnum.MEDICINE_CAT,
+                CatRankEnum.MEDIATOR,
+                CatRankEnum.DEPUTY,
+                CatRankEnum.LEADER,
+                CatRankEnum.ELDER]
+
+    @property
+    def all_outsider_ranks(self) -> list:
+        return [CatRankEnum.LONER, CatRankEnum.ROGUE, CatRankEnum.KITTYPET]
+
+
+class CatStandingEnum(StrEnum):
+    MEMBER = "member"
+    LEFT = "left"
+    LOST = "lost"
+    EXILED = "exiled"
+    KNOWN = "known"
