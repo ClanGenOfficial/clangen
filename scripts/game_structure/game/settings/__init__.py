d4857e8960e8e974ca7fc0eb42e7cada5ff54b61
J Gynn <48025294+j-gynn@users.noreply.github.com>
2025-06-17 17:35:09 +0100

    ...this is embarrassing at this point

--- scripts/game_structure/game/settings/__init__.py
+++ scripts/game_structure/game/settings/__init__.py
@@ -3,5 +3,6 @@ from scripts.game_structure.game.settings.settings import (
     set_game_setting,
     save_game_settings,
     load_game_settings,
+    switch_game_setting,
     game_settings_generator,
 )
