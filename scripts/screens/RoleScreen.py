36626b6f53cdcb3bf843ddc391f008de7fc4d585
scribblecrumb <abspence2199@gmail.com>
2025-06-21 11:41:50 -0500

    update role screen to new status

--- scripts/screens/RoleScreen.py
+++ scripts/screens/RoleScreen.py
@@ -245,7 +245,7 @@ class RoleScreen(Screens):
         )
 
         text = [
-            "<b>" + i18n.t(f"general.{self.the_cat.status}", count=1) + "</b>",
+            "<b>" + i18n.t(f"general.{self.the_cat.status.rank}", count=1) + "</b>",
             i18n.t(f"cat.personality.{self.the_cat.personality.trait}"),
             i18n.t("general.moons_age", count=self.the_cat.moons)
             + "  |  "
@@ -294,21 +294,21 @@ class RoleScreen(Screens):
 
         main_dir = "resources/images/"
         paths = {
-            "leader": "leader_icon.png",
-            "deputy": "deputy_icon.png",
-            "medicine cat": "medic_icon.png",
-            "medicine cat apprentice": "medic_app_icon.png",
-            "mediator": "mediator_icon.png",
-            "mediator apprentice": "mediator_app_icon.png",
-            "warrior": "warrior_icon.png",
-            "apprentice": "warrior_app_icon.png",
-            "kitten": "kit_icon.png",
-            "newborn": "kit_icon.png",
-            "elder": "elder_icon.png",
+            CatRank.LEADER: "leader_icon.png",
+            CatRank.DEPUTY: "deputy_icon.png",
+            CatRank.MEDICINE_CAT: "medic_icon.png",
+            CatRank.MEDICINE_APPRENTICE: "medic_app_icon.png",
+            CatRank.MEDIATOR: "mediator_icon.png",
+            CatRank.MEDIATOR_APPRENTICE: "mediator_app_icon.png",
+            CatRank.WARRIOR: "warrior_icon.png",
+            CatRank.APPRENTICE: "warrior_app_icon.png",
+            CatRank.KITTEN: "kit_icon.png",
+            CatRank.NEWBORN: "kit_icon.png",
+            CatRank.ELDER: "elder_icon.png",
         }
 
-        if self.the_cat.status in paths:
-            icon_path = os.path.join(main_dir, paths[self.the_cat.status])
+        if self.the_cat.status.rank in paths:
+            icon_path = os.path.join(main_dir, paths[self.the_cat.status.rank])
         else:
             icon_path = os.path.join(main_dir, "buttonrank.png")
 
@@ -331,16 +331,16 @@ class RoleScreen(Screens):
         self.update_previous_next_cat_buttons()
 
         if game.clan.leader:
-            leader_invalid = game.clan.leader.dead or game.clan.leader.outside
+            leader_invalid = game.clan.leader.status.in_player_clan()
         else:
             leader_invalid = True
 
         if game.clan.deputy:
-            deputy_invalid = game.clan.deputy.dead or game.clan.deputy.outside
+            deputy_invalid = game.clan.deputy.status.in_player_clan()
         else:
             deputy_invalid = True
 
-        if self.the_cat.status == CatRank.APPRENTICE:
+        if self.the_cat.status.rank == CatRank.APPRENTICE:
             # LEADERSHIP
             self.promote_leader.disable()
             self.promote_deputy.disable()
@@ -355,7 +355,7 @@ class RoleScreen(Screens):
             self.switch_med_app.enable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.enable()
-        elif self.the_cat.status == CatRank.WARRIOR:
+        elif self.the_cat.status.rank == CatRank.WARRIOR:
             # LEADERSHIP
             if leader_invalid:
                 self.promote_leader.enable()
@@ -377,7 +377,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.DEPUTY:
+        elif self.the_cat.status.rank == CatRank.DEPUTY:
             if leader_invalid:
                 self.promote_leader.enable()
             else:
@@ -395,7 +395,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.MEDICINE_CAT:
+        elif self.the_cat.status.rank == CatRank.MEDICINE_CAT:
             self.promote_leader.disable()
             self.promote_deputy.disable()
 
@@ -408,7 +408,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.MEDIATOR:
+        elif self.the_cat.status.rank == CatRank.MEDIATOR:
             if leader_invalid:
                 self.promote_leader.enable()
             else:
@@ -428,7 +428,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.ELDER:
+        elif self.the_cat.status.rank == CatRank.ELDER:
             if leader_invalid:
                 self.promote_leader.enable()
             else:
@@ -449,7 +449,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.disable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.MEDICINE_APPRENTICE:
+        elif self.the_cat.status.rank == CatRank.MEDICINE_APPRENTICE:
             self.promote_leader.disable()
             self.promote_deputy.disable()
 
@@ -463,7 +463,7 @@ class RoleScreen(Screens):
             self.switch_med_app.disable()
             self.switch_warrior_app.enable()
             self.switch_mediator_app.enable()
-        elif self.the_cat.status == CatRank.MEDIATOR_APPRENTICE:
+        elif self.the_cat.status.rank == CatRank.MEDIATOR_APPRENTICE:
             self.promote_leader.disable()
             self.promote_deputy.disable()
 
@@ -477,7 +477,7 @@ class RoleScreen(Screens):
             self.switch_med_app.enable()
             self.switch_warrior_app.enable()
             self.switch_mediator_app.disable()
-        elif self.the_cat.status == CatRank.LEADER:
+        elif self.the_cat.status.rank == CatRank.LEADER:
             self.promote_leader.disable()
             self.promote_deputy.disable()
 
@@ -507,27 +507,27 @@ class RoleScreen(Screens):
             self.switch_mediator_app.disable()
 
     def get_role_blurb(self):
-        if self.the_cat.status == CatRank.WARRIOR:
+        if self.the_cat.status.rank == CatRank.WARRIOR:
             output = "screens.role.blurb_warrior"
-        elif self.the_cat.status == CatRank.LEADER:
+        elif self.the_cat.status.rank == CatRank.LEADER:
             output = "screens.role.blurb_leader"
-        elif self.the_cat.status == CatRank.DEPUTY:
+        elif self.the_cat.status.rank == CatRank.DEPUTY:
             output = "screens.role.blurb_deputy"
-        elif self.the_cat.status == CatRank.MEDICINE_CAT:
+        elif self.the_cat.status.rank == CatRank.MEDICINE_CAT:
             output = "screens.role.blurb_medicine_cat"
-        elif self.the_cat.status == CatRank.MEDIATOR:
+        elif self.the_cat.status.rank == CatRank.MEDIATOR:
             output = "screens.role.blurb_mediator"
-        elif self.the_cat.status == CatRank.ELDER:
+        elif self.the_cat.status.rank == CatRank.ELDER:
             output = "screens.role.blurb_elder"
-        elif self.the_cat.status == CatRank.APPRENTICE:
+        elif self.the_cat.status.rank == CatRank.APPRENTICE:
             output = "screens.role.blurb_apprentice"
-        elif self.the_cat.status == CatRank.MEDICINE_APPRENTICE:
+        elif self.the_cat.status.rank == CatRank.MEDICINE_APPRENTICE:
             output = "screens.role.blurb_medcat_app"
-        elif self.the_cat.status == CatRank.MEDIATOR_APPRENTICE:
+        elif self.the_cat.status.rank == CatRank.MEDIATOR_APPRENTICE:
             output = "screens.role.blurb_mediator_app"
-        elif self.the_cat.status == CatRank.KITTEN:
+        elif self.the_cat.status.rank == CatRank.KITTEN:
             output = "screens.role.blurb_kitten"
-        elif self.the_cat.status == CatRank.NEWBORN:
+        elif self.the_cat.status.rank == CatRank.NEWBORN:
             output = "screens.role.blurb_newborn"
         else:
             output = "screens.role.blurb_unknown"
