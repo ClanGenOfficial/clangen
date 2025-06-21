e05d3e8b1a1f8488186a370350dfe0ca162929f5
scribblecrumb <abspence2199@gmail.com>
2025-06-21 11:42:56 -0500

    update sprite inspect to new status

--- scripts/screens/SpriteInspectScreen.py
+++ scripts/screens/SpriteInspectScreen.py
@@ -15,6 +15,7 @@ from scripts.utility import (
     get_text_box_theme, )
 from scripts.utility import ui_scale
 from .Screens import Screens
+from ..cat.enums import CatGroup
 from ..game_structure.screen_settings import MANAGER
 from ..game_structure.windows import SaveAsImage
 from ..ui.generate_button import get_button_dict, ButtonStyles
@@ -483,7 +484,7 @@ class SpriteInspectScreen(Screens):
         if light_dark == "light":
             offset = 80
 
-        if the_cat.df:
+        if the_cat.status.group == CatGroup.DARK_FOREST:
             biome_platforms = platformsheet.subsurface(
                 pygame.Rect(0, order.index("SC/DF") * 70, 640, 70)
             )
