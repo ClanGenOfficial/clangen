50e3ba0e30db667dedc6b59210b5b19e565624a8
scribblecrumb <abspence2199@gmail.com>
2025-06-23 16:11:56 -0500

    fix exile invite

--- scripts/screens/LeaderDenScreen.py
+++ scripts/screens/LeaderDenScreen.py
@@ -933,7 +933,6 @@ class LeaderDenScreen(Screens):
 
         if (
             self.focus_cat.status.is_outsider()
-            and not self.focus_cat.status.is_exiled(CatGroup.PLAYER_CLAN)
             and not self.focus_cat.status.is_lost(CatGroup.PLAYER_CLAN)
         ):
             self.focus_button["invite"].set_text("screens.leader_den.invite")
