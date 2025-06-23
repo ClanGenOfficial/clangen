837f95588eea8e6efaac266d767aa9e2155ac63c
scribblecrumb <abspence2199@gmail.com>
2025-06-22 21:51:36 -0500

    oopsie, put this back

--- main.py
+++ main.py
@@ -222,12 +222,19 @@ def load_data():
     clan_list = game.read_clans()
     if clan_list:
         game.switches["clan_list"] = clan_list
-        load_cats()
-        version_info = clan_class.load_clan()
-        version_convert(version_info)
-        game.load_events()
-        scripts.screens.screens_core.screens_core.rebuild_core()
-
+        try:
+            load_cats()
+            version_info = clan_class.load_clan()
+            version_convert(version_info)
+            game.load_events()
+            scripts.screens.screens_core.screens_core.rebuild_core()
+        except Exception as e:
+            logging.exception("File failed to load")
+            if not game.switches["error_message"]:
+                game.switches[
+                    "error_message"
+                ] = "There was an error loading the cats file!"
+                game.switches["traceback"] = e
 
     finished_loading = True
 
