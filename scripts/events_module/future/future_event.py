aa14a404acc90d2ab1319eda8735fdcc7261b306
scribblecrumb <abspence2199@gmail.com>
2025-06-26 18:27:27 -0500

    removing target cat
    i just don't think it's that useful to include relationship_status constraints here.  it complicates the event editor and future event format without providing much real utility.

--- scripts/events_module/future/future_event.py
+++ scripts/events_module/future/future_event.py
@@ -59,9 +59,8 @@ def _collect_involved_cats(cat_dict: dict, future_info: dict) -> dict:
     for new_role, cat_involved in future_info["involved_cats"].items():
         # grab any cats that need to be newly gathered
         if isinstance(cat_involved, dict):
-            target_cat = cat_involved.get("target_cat")
             gathered_cat_dict[new_role] = cat_for_event(
-                cat_involved, possible_cats, target_cat
+                cat_involved, possible_cats
             )
             possible_cats.remove(Cat.fetch_cat(gathered_cat_dict[new_role]))
             continue
