54d322e6d1e7cad72f795137d1469a9f0096dc26
scribblecrumb <abspence2199@gmail.com>
2025-06-27 14:38:55 -0500

    fix typo

--- scripts/screens/EventEditScreen.py
+++ scripts/screens/EventEditScreen.py
@@ -865,7 +865,7 @@ class EventEditScreen(Screens):
             # TEXT PREVIEW
             if event.ui_element == self.event_text_element[
                 "preview_button"
-            ] and self.event_text_element.get("event_texta"):
+            ] and self.event_text_element.get("event_text"):
                 # finds what the new preview state should be
                 index = self.preview_states.index(self.current_preview_state)
                 new_index = (index + 1) % 3
