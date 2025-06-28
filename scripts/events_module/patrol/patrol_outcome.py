2d164059180ad17639c0fd4e5705b9e604a22d16
flamedash2358 <lucykooistra@icloud.com>
2025-06-28 16:59:53 -0700

    Update patrol_outcome.py
    Fixing multi death leader death bug

--- scripts/events_module/patrol/patrol_outcome.py
+++ scripts/events_module/patrol/patrol_outcome.py
@@ -516,7 +516,8 @@ class PatrolOutcome:
                     game.clan.leader_lives = 0
                     results.append(
                         event_text_adjust(
-                            Cat, i18n.t("cat.history.leader_death_all"), main_cat=_cat
+                            Cat, i18n.t("cat.history.n_leader_death_all"),
+                            main_cat=_cat
                         )
                     )
                 elif "some_lives" in self.dead_cats:
@@ -525,7 +526,7 @@ class PatrolOutcome:
                     results.append(
                         event_text_adjust(
                             Cat,
-                            i18n.t("cat.history.leader_death_all", count=lives_lost),
+                            i18n.t("cat.history.n_leader_lost_lives", count=lives_lost),
                             main_cat=_cat,
                         )
                     )
@@ -534,7 +535,7 @@ class PatrolOutcome:
                     results.append(
                         event_text_adjust(
                             Cat,
-                            i18n.t("cat.history.leader_death_all", count=1),
+                            i18n.t("cat.history.n_leader_lost_lives", count=1),
                             main_cat=_cat,
                         )
                     )
