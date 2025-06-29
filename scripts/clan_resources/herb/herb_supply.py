acf3cc44981ca97b458fe5198ce0d531086d5e12
flamedash2358 <lucykooistra@icloud.com>
2025-06-29 13:24:35 -0700

    Update herb_supply.py
    CRASHES GRRRR

--- scripts/clan_resources/herb/herb_supply.py
+++ scripts/clan_resources/herb/herb_supply.py
@@ -56,6 +56,9 @@ class HerbSupply:
                 self.storage[herb] = []
             if herb not in self.collected:
                 self.collected[herb] = 0
+            if herb not in self.herb:
+                self.herb[herb] = Herb(herb)
+                print("get OWNED you stinky herb crash")
 
     @property
     def combined_supply_dict(self) -> dict:
