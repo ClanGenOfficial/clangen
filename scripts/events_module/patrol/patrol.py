ffda7dbad3542c694a5f41104110cebc273954c3
scribblecrumb <abspence2199@gmail.com>
2025-07-07 13:32:44 -0500

    fix filter refactor

--- scripts/events_module/patrol/patrol.py
+++ scripts/events_module/patrol/patrol.py
@@ -480,7 +480,7 @@ class Patrol:
     def _check_constraints(self, patrol: PatrolEvent) -> bool:
         if not filter_relationship_type(
             group=self.patrol_cats,
-            filter_list=patrol.relationship_constraints,
+            filter=patrol.relationship_constraints,
             event_id=patrol.patrol_id,
             patrol_leader=self.patrol_leader,
         ):
