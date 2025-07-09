c65a4d1ee4f6658cf054c9254257cb0be6dc04d8
scribblecrumb <abspence2199@gmail.com>
2025-07-09 10:33:32 -0500

    text entry line element

--- scripts/game_structure/editor_elements.py
+++ scripts/game_structure/editor_elements.py
@@ -23,3 +23,44 @@ class EditorElement:
         for ele in self.ui_elements:
             ele.show()
 
+
+class EditorTextEntryLine(EditorElement):
+    def __init__(
+        self,
+        position: Tuple[int, int],
+        anchors: dict = None,
+        container=None,
+        description: str = None,
+        initial_entry_text: str = "",
+        manager=None,
+    ):
+        super().__init__()
+
+        self.description = UITextBoxTweaked(
+            description,
+            ui_scale(pygame.Rect(position, (-1, -1))),
+            object_id=get_text_box_theme("#text_box_30_horizleft_pad_10_10"),
+            line_spacing=1,
+            manager=manager,
+            container=container,
+            anchors=anchors,
+        )
+        self.ui_elements.append(self.description)
+
+        self.initial_entry_text = initial_entry_text
+        self.entry = pygame_gui.elements.UITextEntryLine(
+            ui_scale(pygame.Rect((0, 13), (230, 29))),
+            manager=manager,
+            container=container,
+            anchors={"left_target": self.description},
+            initial_text=self.initial_entry_text,
+        )
+        self.ui_elements.append(self.entry)
+
+    @property
+    def changed(self):
+        if self.entry.text != self.initial_entry_text:
+            # save new text state to compare when next changed
+            self.initial_entry_text = self.entry.text
+            return True
+        return False
