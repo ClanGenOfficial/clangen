277f36531f488cb49106e05ff75ce7c9fd9d7b06
scribblecrumb <abspence2199@gmail.com>
2025-07-10 14:02:19 -0500

    formatting

--- scripts/cat/history.py
+++ scripts/cat/history.py
@@ -440,7 +440,9 @@ class History:
         if "is_victim" not in victim.history.murder:
             victim.history.murder["is_victim"] = []
         else:
-            print(f"WARNING: victim cat: {victim.ID} already has a murder history - as the victim!")
+            print(
+                f"WARNING: victim cat: {victim.ID} already has a murder history - as the victim!"
+            )
 
         self.murder["is_murderer"].append(
             {
