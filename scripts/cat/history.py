576f1f7c8df7fc14dae8efb99020b051f599ffee
scribblecrumb <abspence2199@gmail.com>
2025-07-10 12:34:57 -0500

    review suggestion

--- scripts/cat/history.py
+++ scripts/cat/history.py
@@ -497,17 +497,15 @@ class History:
         """
         text = ""
         if murder["revealed"]["to_clan"]:
-            text = i18n.t("cat.history.murder_revealed_to_clan")
-        else:
-            if murder["revealed"]["aware_individuals"]:
-                individuals = [
-                    Cat.fetch_cat(c).name
-                    for c in murder["revealed"]["aware_individuals"]
-                ]
-                names = adjust_list_text(individuals)
-                text = f"{i18n.t('cat.history.murder_revealed_to_individual', name=names)} "
+            return i18n.t("cat.history.murder_revealed_to_clan")
+        if murder["revealed"]["aware_individuals"]:
+            individuals = [
+                Cat.fetch_cat(c).name for c in murder["revealed"]["aware_individuals"]
+            ]
+            names = adjust_list_text(individuals)
+            text = f"{i18n.t('cat.history.murder_revealed_to_individual', name=names)} "
 
-            text += i18n.t("cat.history.murder_not_revealed_to_clan")
+        text += i18n.t("cat.history.murder_not_revealed_to_clan")
         return text
 
     def add_lead_ceremony(self):
