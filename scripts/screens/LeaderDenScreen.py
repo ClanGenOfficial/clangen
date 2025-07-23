a0aa69d865edf4d12824777e19e56315416f3e4f
Lara <63805048+larkgz@users.noreply.github.com>
2025-07-23 16:26:30 -0400

    fix: don't show own clan

--- scripts/screens/LeaderDenScreen.py
+++ scripts/screens/LeaderDenScreen.py
@@ -432,7 +432,7 @@ class LeaderDenScreen(Screens):
             manager=MANAGER,
         )
         for i, other_clan in enumerate(game.clan.all_clans):
-            if other_clan.name == game.clan.name:
+            if other_clan.name == game.clan.displayname:
                 continue
             x_pos = 128
             self.other_clan_selection_elements[f"container{i}"] = UIContainer(
