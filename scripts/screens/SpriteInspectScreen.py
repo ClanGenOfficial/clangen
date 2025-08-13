5e54ace93ad5188219832eba3d9dee9ee0bb0c7b
scribblecrumb <abspence2199@gmail.com>
2025-08-12 20:10:59 -0500

    fix merge

--- scripts/screens/SpriteInspectScreen.py
+++ scripts/screens/SpriteInspectScreen.py
@@ -19,9 +19,7 @@ from .Screens import Screens
 from .enums import GameScreen
 from ..cat.sprites import sprites
 from ..clan_package.settings import get_clan_setting
-from ..game_structure.game.settings import game_setting_get
 from ..game_structure.game.switches import switch_set_value, switch_get_value, Switch
-from ..cat.enums import CatGroup
 from ..game_structure.screen_settings import MANAGER
 from ..game_structure.windows import SaveAsImage
 from ..ui.generate_button import get_button_dict, ButtonStyles
