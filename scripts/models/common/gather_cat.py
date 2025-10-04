e314be78b4e01e21636d53e7ea4c124ff1da2093
Philipp Micke <archanyhm@archanyhm.dev>
2025-10-04 17:03:33 +0200

    feat: add pydantic models for common schema

---
+++ scripts/models/common/gather_cat.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from typing import Union, Annotated
+
+from pydantic import StringConstraints, RootModel
+
+
+class GatherCatEnum(Enum):
+    m_c = "m_c"
+    r_c = "r_c"
+    p_l = "p_l"
+    s_c = "s_c"
+    app1 = "app1"
+    app2 = "app2"
+    app3 = "app3"
+    app4 = "app4"
+    app5 = "app5"
+    app6 = "app6"
+    clan = "clan"
+    some_clan = "some_clan"
+    patrol = "patrol"
+    multi = "multi"
+
+
+class GatherCat(RootModel):
+    root: Union[
+        GatherCatEnum, Annotated[str, StringConstraints(pattern=r"^n_c:[0-9]+$")]
+    ]
