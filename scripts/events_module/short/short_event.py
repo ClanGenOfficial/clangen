e0b6fbb69423bc6f1c075df558f6505f46f026f2
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2025-10-16 18:55:48 +0300

    swap to .clear()-ing lists

--- scripts/events_module/short/short_event.py
+++ scripts/events_module/short/short_event.py
@@ -217,10 +217,10 @@ class ShortEvent:
         """
         self.additional_event_text = ""
         self.text = self.text_template
-        self.all_involved_cat_ids: list[int] = []
-        self.new_cats = []
-        self.multi_cat_objects = []
-        self.dead_cat_objects = []
+        self.all_involved_cat_ids.clear()
+        self.new_cats.clear()
+        self.multi_cat_objects.clear()
+        self.dead_cat_objects.clear()
 
         if other_clan:
             self.other_clan_name = f"{other_clan.name}Clan"
