a03e3f21730234de4e17ab386e42f0adf947b4f9
Rusty <rusty3925@gmail.com>
2025-11-17 12:08:05 -0800

    fix: localize graduation moon text (#3567)

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -1552,7 +1552,9 @@ class ProfileScreen(Screens):
                 )
 
             if switch_get_value(Switch.show_history_moons):
-                graduation_history += f" (moon {app_ceremony['moon']})"
+                graduation_history += i18n.t(
+                    "cat.history.graduation_moon", moon=app_ceremony["moon"]
+                )
         cat_dict = {"m_c": (str(self.the_cat.name), choice(self.the_cat.pronouns))}
         apprenticeship_history = influence_history + " " + graduation_history
         apprenticeship_history = process_text(apprenticeship_history, cat_dict)
