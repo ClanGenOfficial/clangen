12bd4c8330d0bbd0005b7d623658216c5f00446a
scribblecrumb <abspence2199@gmail.com>
2025-12-28 14:52:44 -0600

    update delays

--- scripts/game_structure/audio/music.py
+++ scripts/game_structure/audio/music.py
@@ -195,7 +195,7 @@ class Music:
             self._stop_timers()
             self._start_music_timer(self.remaining_time_of_paused_track)
 
-    def fade_out(self, fadeout=2000, delay=randint(120, 300)):
+    def fade_out(self, fadeout=2000, delay=randint(30, 300)):
         """
         fades the music out and begins the silence timer to count down to next track play
         :param fadeout: length of fadeout in milliseconds
@@ -233,7 +233,7 @@ class Music:
         self.music_timer.daemon = True
         self.music_timer.start()
 
-    def _start_silence_timer(self, duration=randint(120, 300)):
+    def _start_silence_timer(self, duration=randint(30, 300)):
         """
         Clears old music, then sets a timer for the next track to play.  When the timer ends, new music begins.
         :param duration: length of silence in seconds, by default this is a random duration between 120 and 300 seconds
