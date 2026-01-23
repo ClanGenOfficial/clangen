d71a0a064185648b89240d584d9f40c581b320d4
J Gynn <48025294+j-gynn@users.noreply.github.com>
2026-01-23 20:22:17 +0000

    Removed get_num_of_cats_with_relation_amount_towards

--- scripts/utility.py
+++ scripts/utility.py
@@ -32,42 +32,6 @@ if TYPE_CHECKING:
 # ---------------------------------------------------------------------------- #
 
 
-# todo: kill this function. with fire.
-def get_num_of_cats_with_relation_amount_towards(cat, amount, all_cats):
-    """
-    Looks how many cats have the certain value
-    :param cat: cat in question
-    :param amount: amount of relationship value which has to be reached
-    :param all_cats: list of cats which has to be checked
-    """
-
-    # collect all true or false if the value is reached for the cat or not
-    # later count or sum can be used to get the amount of cats
-    # this will be handled like this, because it is easier / shorter to check
-
-    relation_dict = {v: [] for v in [*RelType]}
-
-    for inter_cat in all_cats:
-        if cat.ID in inter_cat.relationships:
-            relation = inter_cat.relationships[cat.ID]
-        else:
-            continue
-
-        for value in [*RelType]:
-            if amount > 0:
-                relation_dict[value].append(
-                    relation.get_amount_of_type(value) >= amount
-                )
-            elif amount < 0:
-                relation_dict[value].append(
-                    relation.get_amount_of_type(value) <= amount
-                )
-
-    return_dict = {v: sum(relation_dict[v]) for v in [*RelType]}
-
-    return return_dict
-
-
 # ---------------------------------------------------------------------------- #
 #                               Text Adjust                                    #
 # ---------------------------------------------------------------------------- #
