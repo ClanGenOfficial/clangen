bd06757b03d4ecfa9a3fe5d4be57d2632bdcf7b5
scribblecrumb <abspence2199@gmail.com>
2026-01-26 08:42:01 -0600

    add: cat list button to no_mediator window

--- scripts/ui/windows/no_mediator.py
+++ scripts/ui/windows/no_mediator.py
@@ -18,14 +18,14 @@ from scripts.utility import ui_scale
 class NoMediatorsWindow(GameWindow):
     def __init__(self):
         super().__init__(
-            ui_scale(pygame.Rect((300, 200), (200, 150))),
+            ui_scale(pygame.Rect((300, 200), (250, 170))),
             window_display_title="No Mediators",
             click_outside_to_close=False,
         )
 
         self.missing_info = UITextBoxTweaked(
             "windows.no_mediators",
-            ui_scale(pygame.Rect((0, 30), (200, -1))),
+            ui_scale(pygame.Rect((0, 30), (220, -1))),
             line_spacing=1,
             manager=MANAGER,
             object_id="#text_box_30_horizcenter",
@@ -36,8 +36,18 @@ class NoMediatorsWindow(GameWindow):
         )
 
         self.return_button = UISurfaceImageButton(
-            ui_scale(pygame.Rect((0, 100), (105, 30))),
+            ui_scale(pygame.Rect((0, 80), (105, 30))),
             "buttons.back",
+            get_button_dict(ButtonStyles.SQUOVAL, (105, 30)),
+            object_id="@buttonstyles_squoval",
+            manager=MANAGER,
+            container=self,
+            anchors={"centerx": "centerx"},
+        )
+
+        self.cat_list_button = UISurfaceImageButton(
+            ui_scale(pygame.Rect((0, 115), (105, 30))),
+            "buttons.cat_list",
             get_button_dict(ButtonStyles.SQUOVAL, (105, 30)),
             object_id="@buttonstyles_squoval",
             manager=MANAGER,
@@ -49,6 +59,11 @@ class NoMediatorsWindow(GameWindow):
         if event.type == pygame_gui.UI_BUTTON_START_PRESS:
             if event.ui_element in (self.return_button, self.back_button):
                 switch_set_value(Switch.cur_screen, game.last_screen_forupdate)
+                game.last_screen_forupdate = GameScreen.MEDIATION
+                game.switch_screens = True
+                self.kill()
+            if event.ui_element == self.cat_list_button:
+                switch_set_value(Switch.cur_screen, GameScreen.LIST)
                 game.last_screen_forupdate = GameScreen.MEDIATION
                 game.switch_screens = True
                 self.kill()
