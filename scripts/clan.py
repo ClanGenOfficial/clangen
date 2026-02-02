ecf1c341a29e3e143bb0f6c8ad24654520fcceb2
Camden Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-02-01 20:38:41 -0600

    Enhance clan function with parameter docstring
    Add docstring to clan function parameters.

--- scripts/clan.py
+++ scripts/clan.py
@@ -74,6 +74,10 @@ class Clan:
         self_run_init_functions=True,
         name="",
     ):
+        """
+        :param save_id: The save file name for the Clan, this should not be used for player-facing text beyond the save file screen
+        :param name: The display name for the Clan, this is what should appear while the playing the game.
+        """
         if save_id == "":
             return
 
