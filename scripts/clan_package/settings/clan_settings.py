bf4d64013c29124d00aa2b746e315002ac77c1c5
Braidon <braidonaksingh@gmail.com>
2026-02-12 23:28:13 -0600

    formatting

--- scripts/clan_package/settings/clan_settings.py
+++ scripts/clan_package/settings/clan_settings.py
@@ -85,7 +85,9 @@ def reset_loaded_clan_settings():
 # Init Settings
 clan_settings = {}
 _clan_settings = DISPLAY_SETTINGS["clan"]
-with open("resources/clansettings_conversion.json", "r", encoding="utf-8") as conversion_file:
+with open(
+    "resources/clansettings_conversion.json", "r", encoding="utf-8"
+) as conversion_file:
     _old_save_conversion = ujson.loads(conversion_file.read())
 
 all_settings = [
