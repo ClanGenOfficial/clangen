10b93b9f5d31bbaf7616f0f07a946aa3b5431bd1
scribblecrumb <abspence2199@gmail.com>
2026-02-15 15:51:12 -0600

    remove cat tests

--- tests/test_event_filters.py
+++ tests/test_event_filters.py
@@ -24,16 +24,6 @@ class TestEventFilters(unittest.TestCase):
         self.test_cat = create_cat(CatRank.LEADER, moons=50)
         game.clan.leader = self.test_cat
 
-        self.cat_constraint_dict = {
-            "age": [],
-            "status": [],
-            "status_history": [],
-            "trait": [],
-            "skill": [],
-            "backstory": [],
-            "gender": [],
-        }
-
     def test_location(self):
         """
         Tests a variety of location tag combinations
@@ -41,216 +31,144 @@ class TestEventFilters(unittest.TestCase):
 
         self.assertTrue(
             event_for_location(locations=["forest"]),
-            "Assert single biome match failed.",
+            "Assert single biome match.",
         )
         self.assertTrue(
             event_for_location(locations=["forest:camp1"]),
-            "Assert single camp match failed.",
+            "Assert single camp match.",
         )
         self.assertFalse(
             event_for_location(locations=["desert"]),
-            "Assert single biome mismatch failed.",
+            "Assert single biome mismatch.",
         )
         self.assertFalse(
             event_for_location(locations=["forest:camp2"]),
-            "Assert single camp mismatch failed.",
+            "Assert single camp mismatch.",
         )
         self.assertTrue(
             event_for_location(locations=["desert:camp2", "forest"]),
-            "Assert mixed location match failed.",
+            "Assert mixed location match.",
         )
         self.assertFalse(
             event_for_location(locations=["desert:camp2", "plains", "forest:camp2"]),
-            "Assert mixed location mismatch failed.",
+            "Assert mixed location mismatch.",
         )
         self.assertTrue(
             event_for_location(locations=["-plains"]),
-            "Assert single location excluded failed.",
+            "Assert single location excluded.",
         )
         self.assertTrue(
             event_for_location(locations=["-plains", "-forest:camp2"]),
-            "Assert mixed locations excluded failed.",
+            "Assert mixed locations excluded.",
         )
         self.assertFalse(
             event_for_location(locations=["-plains", "-forest"]),
-            "Assert mixed locations, including current biome, excluded failed.",
+            "Assert mixed locations, including current biome, excluded.",
         )
         self.assertFalse(
             event_for_location(locations=["-plains", "-forest:camp1"]),
-            "Assert mixed locations, including current camp, excluded failed.",
+            "Assert mixed locations, including current camp, excluded.",
         )
 
     def test_season(self):
         self.assertTrue(
-            event_for_season(["newleaf"]), "Assert single season match failed."
+            event_for_season(["newleaf"]), "Assert single season match."
         )
         self.assertFalse(
-            event_for_season(["greenleaf"]), "Assert single season mismatch failed."
+            event_for_season(["greenleaf"]), "Assert single season mismatch."
         )
         self.assertTrue(
             event_for_season(["newleaf", "greenleaf"]),
-            "Assert mixed season match failed.",
+            "Assert mixed season match.",
         )
         self.assertFalse(
             event_for_season(["greenleaf", "leaf-bare"]),
-            "Assert mixed season mismatch failed.",
+            "Assert mixed season mismatch.",
         )
         self.assertTrue(
             event_for_season(["-greenleaf"]),
-            "Assert single season excluded failed.",
+            "Assert single season excluded.",
         )
         self.assertFalse(
             event_for_season(["-newleaf"]),
-            "Assert current season excluded failed.",
+            "Assert current season excluded.",
         )
         self.assertTrue(
             event_for_season(["-greenleaf", "-leaf-bare"]),
-            "Assert mixed season excluded failed.",
+            "Assert mixed season excluded.",
         )
         self.assertFalse(
             event_for_season(["-greenleaf", "-newleaf"]),
-            "Assert mixed season, including current, excluded failed.",
+            "Assert mixed season, including current, excluded.",
         )
 
     def test_game_mode_tags(self):
         self.assertTrue(
             event_for_tags(["classic"], self.test_cat),
-            "Assert correct game mode tag failed.",
+            "Assert correct game mode tag.",
         )
         self.assertFalse(
             event_for_tags(["expanded"], self.test_cat),
-            "Assert incorrect game mode tag failed",
+            "Assert incorrect game mode tag",
         )
 
     def test_leader_life_tags(self):
         game.clan.leader_lives = 9
         self.assertTrue(
             event_for_tags(["some_lives", "lives_remain", "high_lives"], self.test_cat),
-            "Assert 9-life leader passes some_lives, lives_remain, and high_lives failed.",
+            "Assert 9-life leader passes some_lives, lives_remain, and high_lives.",
         )
         self.assertFalse(
             event_for_tags(["mid_lives", "low_lives"], self.test_cat),
-            "Assert 9-lives leader does not pass mid_lives and low_lives failed.",
+            "Assert 9-lives leader does not pass mid_lives and low_lives.",
         )
         self.assertFalse(
             event_for_tags(["mid_lives", "low_lives", "some_lives"], self.test_cat),
-            "Assert 9-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others failed.",
+            "Assert 9-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others.",
         )
 
         game.clan.leader_lives = 5
         self.assertTrue(
             event_for_tags(["some_lives", "mid_lives", "lives_remain"], self.test_cat),
-            "Assert 5-lives leader passes some_lives, mid_lives, and lives_remain failed.",
+            "Assert 5-lives leader passes some_lives, mid_lives, and lives_remain.",
         )
         self.assertFalse(
             event_for_tags(["high_lives", "low_lives"], self.test_cat),
-            "Assert 5-lives leader does not pass mid_lives and low_lives failed.",
+            "Assert 5-lives leader does not pass mid_lives and low_lives.",
         )
         self.assertFalse(
             event_for_tags(["high_lives", "low_lives", "some_lives"], self.test_cat),
-            "Assert 5-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others failed.",
+            "Assert 5-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others.",
         )
 
         game.clan.leader_lives = 3
         self.assertTrue(
             event_for_tags(["low_lives", "lives_remain"], self.test_cat),
-            "Assert 3-lives leader passes low_lives and lives_remain failed.",
+            "Assert 3-lives leader passes low_lives and lives_remain.",
         )
         self.assertFalse(
             event_for_tags(["high_lives", "mid_lives", "some_lives"], self.test_cat),
-            "Assert 3-lives leader does not pass mid_lives, high_lives, and some_lives failed.",
+            "Assert 3-lives leader does not pass mid_lives, high_lives, and some_lives.",
         )
         self.assertFalse(
             event_for_tags(["high_lives", "low_lives", "some_lives"], self.test_cat),
-            "Assert 3-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others failed.",
+            "Assert 3-lives leader does not pass mixed tag list where they qualify for 1 tag, but not others.",
         )
 
         game.clan.leader_lives = 1
         self.assertTrue(
             event_for_tags(["low_lives"], self.test_cat),
-            "Assert 1-life leader passes low_lives failed.",
+            "Assert 1-life leader passes low_lives.",
         )
         self.assertFalse(
             event_for_tags(
                 ["high_lives", "mid_lives", "some_lives", "lives_remain"], self.test_cat
             ),
-            "Assert 1-life leader does not pass mid_lives, high_lives, some_lives, and lives_remain failed.",
+            "Assert 1-life leader does not pass mid_lives, high_lives, some_lives, and lives_remain.",
         )
         self.assertFalse(
             event_for_tags(["high_lives", "low_lives", "some_lives"], self.test_cat),
-            "Assert 1-life leader does not pass mixed tag list where they qualify for 1 tag, but not others failed.",
+            "Assert 1-life leader does not pass mixed tag list where they qualify for 1 tag, but not others.",
         )
 
-    def test_cat_age(self):
-        self.cat_constraint_dict["age"] = ["adult"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single age match failed.",
-        )
 
-        self.cat_constraint_dict["age"] = ["young adult"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single age mismatch failed.",
-        )
-
-        self.cat_constraint_dict["age"] = ["young adult", "adult"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed age match failed.",
-        )
-
-        self.cat_constraint_dict["age"] = ["young adult", "senior adult"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed age mismatch failed.",
-        )
-
-        self.cat_constraint_dict["age"] = ["-newborn"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single age excluded failed.",
-        )
-
-        self.cat_constraint_dict["age"] = ["-newborn", "-adult"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed age excluded, including current, failed.",
-        )
-
-    def test_cat_status(self):
-        self.cat_constraint_dict["status"] = ["leader"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single status match failed.",
-        )
-
-        self.cat_constraint_dict["status"] = ["warrior"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single status mismatch failed.",
-        )
-
-        self.cat_constraint_dict["status"] = ["leader", "warrior"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed status match failed.",
-        )
-
-        self.cat_constraint_dict["status"] = ["warrior", "medicine cat"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed status mismatch failed.",
-        )
-
-        self.cat_constraint_dict["status"] = ["-warrior"]
-        self.assertTrue(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert single status excluded failed.",
-        )
-
-        self.cat_constraint_dict["status"] = ["-leader", "-warrior"]
-        self.assertFalse(
-            event_for_cat(self.cat_constraint_dict, self.test_cat),
-            "Assert mixed status excluded, including current, failed.",
-        )
