da0f8276670a06ab8e5b568c993c83ce16694228
J Gynn <48025294+j-gynn@users.noreply.github.com>
2026-02-28 21:24:40 +0000

    bugfix: ensure tests always use english cat names

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -3,10 +3,12 @@ import unittest
 from unittest.mock import patch
 from copy import deepcopy
 
+import i18n
+
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
 
-from scripts.game_structure import game
+from scripts.game_structure import game, constants
 
 from scripts.cat.cats import Cat
 from scripts.cat.enums import CatAge, CatRank, CatGroup, CatSocial
@@ -441,6 +443,7 @@ class TestNameRepr(unittest.TestCase):
     def setUpClass(cls):
         os.environ["SDL_VIDEODRIVER"] = "dummy"
         os.environ["SDL_AUDIODRIVER"] = "dummy"
+        constants.CONFIG["cat_name_controls"]["always_use_english"] = True
 
     def test_clancats(self):
         """
