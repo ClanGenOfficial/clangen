0795e2f1dceefaabfc4f6bae56402981c7401ff5
scribblecrumb <abspence2199@gmail.com>
2026-03-10 20:38:48 -0500

    fix formatting
    idk why black decided it hates this

--- scripts/screens/ListScreen.py
+++ scripts/screens/ListScreen.py
@@ -712,7 +712,7 @@ class ListScreen(Screens):
         if self.current_group == "your_clan":
             group = self.clan_name
             first_temper, second_temper = game.clan.temperament
-            temper = f"{i18n.t(f"screens.leader_den.{first_temper}")} & {i18n.t(f"screens.leader_den.{second_temper}")}"
+
         else:
             if self.current_group == "dark_forest":
                 group = i18n.t(f"general.the_dark_forest")
@@ -720,10 +720,12 @@ class ListScreen(Screens):
                 group = i18n.t(f"general.{self.current_group}")
             if self.current_group == "starclan":
                 first_temper, second_temper = game.starclan.temperament
-                temper = f"{i18n.t(f"screens.leader_den.{first_temper}")} & {i18n.t(f"screens.leader_den.{second_temper}")}"
             else:
                 first_temper, second_temper = game.dark_forest.temperament
-                temper = f"{i18n.t(f"screens.leader_den.{first_temper}")} & {i18n.t(f"screens.leader_den.{second_temper}")}"
+
+        first = i18n.t(f"screens.leader_den.{first_temper}")
+        second = i18n.t(f"screens.leader_den.{second_temper}")
+        temper = f"{first} & {second}"
 
         return i18n.t("screens.list.temper", group=group, temper=temper)
 
