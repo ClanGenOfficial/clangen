de761ffbb22d8daf6ae15c81231fb1a00c8c66ba
Lara <63805048+larkgz@users.noreply.github.com>
2026-03-12 22:44:11 -0400

    refactor: merge files

--- scripts/game_input/__init__.py
+++ scripts/game_input/__init__.py
@@ -1,7 +1,6 @@
 from scripts.game_input.action import Action
 from scripts.game_input.custom_events import BUTTON_PRESSED, BUTTON_RELEASED, INPUT_ACTION_PRESSED
-from scripts.game_input.controller import controller_manager
-from scripts.game_input.keyboard import keyboard_manager
+from scripts.game_input.input_manager import keyboard_manager, controller_manager
 
 __all__ = [
   "Action",
--- scripts/game_input/input_manager.py
+++ scripts/game_input/input_manager.py
@@ -5,10 +5,71 @@ import pygame
 from pygame._sdl2 import controller
 
 from typing import Literal, Union
+from abc import ABC, abstractmethod
 
-from scripts.game_input.input_abc import InputManager
+from scripts.game_structure.game.settings import game_setting_get
 from scripts.game_input.action import Action
 from scripts.game_input import custom_events
+
+class InputManager(ABC):
+    """
+    Abstract class for input reading.
+    """
+
+    @abstractmethod
+    def _get_action_from_event(self, event: pygame.Event) -> Union[Action, None]:
+        """
+        :param event: Event to get corresponding Action of.
+        :return: Corresponding Action, or `None` if there's no corresponding Action.
+        """
+
+    def _post_action(self, action: Union[Action, None], event: int):
+        """
+        Posts Action to Pygame events.
+        Event should be one of INPUT_ACTION_PRESSED, BUTTON_PRESSED or BUTTON_RELEASED.
+        """
+        posted_event = pygame.event.Event(event, {"action": action})
+        pygame.event.post(posted_event)
+
+    @abstractmethod
+    def process_event(self, event: pygame.Event):
+        """
+        :param event: Pygame Event to process.
+        """
+
+class KeyboardManager(InputManager):
+    """
+    Input manager for keyboard inputs.
+    """
+
+    action_map = {
+        pygame.K_ESCAPE: Action.BACK,
+        pygame.K_RETURN: Action.CONFIRM,
+        pygame.K_LEFT: Action.PREVIOUS,
+        pygame.K_RIGHT: Action.NEXT,
+        pygame.K_SPACE: Action.SAVE,
+        pygame.K_UP: Action.UP,
+        pygame.K_DOWN: Action.DOWN,
+    }
+
+    def __init__(self):
+        pass
+
+    def _get_action_from_event(self, event: pygame.Event) -> Union[Action, None]:
+        """
+        :param event: Event to get corresponding Action of.
+        :return: Corresponding Action, or `None` if there's no corresponding Action.
+        """
+        return KeyboardManager.action_map.get(event.key)
+
+    def process_event(self, event: pygame.Event):
+        """
+        :param event: Pygame Event to process.
+        """
+        if event.type == pygame.KEYDOWN and game_setting_get("keybinds"):
+            action = self._get_action_from_event(event)
+            if action:
+                self._post_action(action, custom_events.INPUT_ACTION_PRESSED)
 
 class ControllerManager(InputManager):
     """
@@ -181,3 +242,4 @@ class ControllerManager(InputManager):
 
 
 controller_manager = ControllerManager()
+keyboard_manager = KeyboardManager()
