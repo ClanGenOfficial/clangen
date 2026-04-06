6425bd39a31eba525e2820bad48ecc8a7a05b0fb
Maxxicloudd <xxxpinkdrawsxxx@gmail.com>
2026-04-06 17:56:24 -0500

    Ok its formatted stop yelling at me

--- scripts/events_module/short/condition_events.py
+++ scripts/events_module/short/condition_events.py
@@ -812,7 +812,7 @@ class Condition_Events:
                             )
                         ]
                         del translated_condition, translated_injury
-                    
+
                     # choose event string and ensure Clan's med cat number aligns with event text
 
                     med_list = find_alive_cats_with_rank(
