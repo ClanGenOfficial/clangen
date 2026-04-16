130d3b766d252335835ad2ad8382d507bf7ee76b
scribblecrumb <abspence2199@gmail.com>
2026-04-16 17:20:27 -0500

    formatting

--- scripts/events_module/relationship/relation_events.py
+++ scripts/events_module/relationship/relation_events.py
@@ -181,7 +181,9 @@ class Relation_Events:
             chosen_type = "all"
         possible_interaction_cats = list(
             filter(
-                lambda cat: (cat.status.alive_in_player_clan and not cat.age == CatAge.NEWBORN),
+                lambda cat: (
+                    cat.status.alive_in_player_clan and not cat.age == CatAge.NEWBORN
+                ),
                 Cat.all_cats.values(),
             )
         )
