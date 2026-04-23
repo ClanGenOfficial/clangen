77cb95e4491838cfd6e8c9f78571becb851102ab
Lara <63805048+larkgz@users.noreply.github.com>
2026-04-23 16:02:35 -0400

    fix(model): update death field

--- scripts/models/shortevent/history_text.py
+++ scripts/models/shortevent/history_text.py
@@ -16,13 +16,9 @@ class HistoryText(BaseModel):
     ] = Field(
         MISSING, description="List of cats for whom the history will be assigned."
     )
-    reg_death: Union[str, MISSING] = Field(
+    death: Union[str, MISSING] = Field(
         MISSING,
-        description="Death history text for non-leaders. Whole sentence. Must be included if the cat is dead or injured.",
-    )
-    lead_death: Union[str, MISSING] = Field(
-        MISSING,
-        description="Death history text for leaders. Sentence fragment. Must be included if the dead or injured cat could be the leader.",
+        description="Death history text. Whole sentence. Must be included if the cat is dead or injured.",
     )
     scar: Union[str, MISSING] = Field(
         MISSING,
