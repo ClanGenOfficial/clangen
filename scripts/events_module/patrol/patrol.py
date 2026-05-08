e960f489215f3db864c475309770dc0bf6d568ec
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-08 18:56:32 -0400

    feat: add patrol art

--- scripts/events_module/patrol/patrol.py
+++ scripts/events_module/patrol/patrol.py
@@ -40,6 +40,8 @@ from scripts.events_module.text_adjust import (
     adjust_list_text,
     event_text_adjust,
 )
+from scripts.special_dates import SpecialDate, is_today
+
 
 logger = logging.getLogger(__name__)
 
@@ -1198,6 +1200,11 @@ class Patrol:
                 file_name = "train"
 
             file_name = f"{file_name}_general_intro"
+
+        if is_today(SpecialDate.APRIL_FOOLS):
+            april_fools_root_dir = "resources/images/patrol_art/april_fools/"
+            if path_exists(f"{april_fools_root_dir}{file_name}.png"):
+                return pygame.image.load(f"{april_fools_root_dir}{file_name}.png")
 
         return pygame.image.load(f"{root_dir}{file_name}.png")
 
