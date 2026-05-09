1457944c981ef905aa28345b2022d865c6433fe3
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-09 19:09:41 -0400

    fix: import crash

--- scripts/game_input/__init__.py
+++ scripts/game_input/__init__.py
@@ -1,15 +1,13 @@
 from scripts.game_input.action import Action
 from scripts.game_input.custom_events import (
-    BUTTON_PRESSED,
-    BUTTON_RELEASED,
+    INPUT_ACTION_RELEASED,
     INPUT_ACTION_PRESSED,
 )
 from scripts.game_input.input_manager import keyboard_manager, controller_manager
 
 __all__ = [
     "Action",
-    "BUTTON_PRESSED",
-    "BUTTON_RELEASED",
+    "INPUT_ACTION_RELEASED",
     "INPUT_ACTION_PRESSED",
     "controller_manager",
     "keyboard_manager",
