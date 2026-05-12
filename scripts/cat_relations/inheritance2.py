6043d77b61a8ad8215433974e0291679a904af7f
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-11 22:25:46 -0400

    refactor: use dataclass instead of typeddict

--- scripts/cat_relations/inheritance2.py
+++ scripts/cat_relations/inheritance2.py
@@ -1,6 +1,7 @@
 from scripts.cat.cats import Cat
 from typing import TypedDict, Dict, List, Literal, Set
 from enum import StrEnum
+from dataclasses import dataclass, field
 
 
 class RelationType(StrEnum):
@@ -19,14 +20,19 @@ class FamilyRelationLink(TypedDict):
     cat_id: str
 
 
+@dataclass
+class FamilyRelations:
+    parents: List[FamilyRelationLink] = field(default_factory=lambda: [])
+    children: List[FamilyRelationLink] = field(default_factory=lambda: [])
+    siblings: List[FamilyRelationLink] = field(default_factory=lambda: [])
+    mates: List[FamilyRelationLink] = field(default_factory=lambda: [])
+
+
 class InheritanceDb:
     def __init__(self):
         self._cat_to_rel: Dict[
             str,
-            Dict[
-                Literal["parents", "children", "siblings", "mates"],
-                List[FamilyRelationLink],
-            ],
+            FamilyRelations,
         ] = {}
 
     def __getitem__(self, arg: str):
@@ -38,41 +44,33 @@ class InheritanceDb:
     def load_inheritances(self):
         cat_to_rel: Dict[
             str,
-            Dict[
-                Literal["parents", "children", "siblings", "mates"],
-                List[FamilyRelationLink],
-            ],
+            FamilyRelations,
         ] = {}
 
         # add parents
         for c in Cat.all_cats_list:
             if c.ID not in cat_to_rel:
-                cat_to_rel[c.ID] = {
-                    "parents": [],
-                    "children": [],
-                    "siblings": [],
-                    "mates": [],
-                }
+                cat_to_rel[c.ID] = FamilyRelations()
 
             for p in c.adoptive_parents:
-                cat_to_rel[c.ID]["parents"].append(
+                cat_to_rel[c.ID].parents.append(
                     {"relation_type": RelationType.ADOPTIVE, "cat_id": p}
                 )
             for p in [c.parent1, c.parent2]:
                 if p:
-                    cat_to_rel[c.ID]["parents"].append(
+                    cat_to_rel[c.ID].parents.append(
                         {"relation_type": RelationType.BLOOD, "cat_id": p}
                     )
 
             for m in c.mate:
-                cat_to_rel[c.ID]["mates"].append(
+                cat_to_rel[c.ID].mates.append(
                     {"relation_type": RelationType.NOT_BLOOD, "cat_id": m}
                 )
 
         # add kits
         # in a separate loop so that we're sure we already know about their parents
         for c in Cat.all_cats_list:
-            for parent_rel in cat_to_rel[c.ID]["parents"]:
+            for parent_rel in cat_to_rel[c.ID].parents:
                 parent_id = parent_rel["cat_id"]
                 parent = Cat.fetch_cat(parent_id)
                 if parent:
@@ -85,25 +83,20 @@ class InheritanceDb:
                     if (
                         parent_id not in cat_to_rel
                     ):  # might not exist if you're faded, so we have to do. this.
-                        cat_to_rel[parent_id] = {
-                            "parents": [],
-                            "children": [],
-                            "siblings": [],
-                            "mates": [],
-                        }
+                        cat_to_rel[parent_id] = FamilyRelations()
 
-                    cat_to_rel[parent_id]["children"].append(rel)
+                    cat_to_rel[parent_id].children.append(rel)
 
         self._cat_to_rel = cat_to_rel
 
     def get_parents(self, cat_id: str) -> Set[str]:
-        return {p["cat_id"] for p in self._cat_to_rel[cat_id]["parents"]}
+        return {p["cat_id"] for p in self._cat_to_rel[cat_id].parents}
 
     def get_mates(self, cat_id: str) -> Set[str]:
-        return {m["cat_id"] for m in self._cat_to_rel[cat_id]["mates"]}
+        return {m["cat_id"] for m in self._cat_to_rel[cat_id].mates}
 
     def get_children(self, cat_id: str) -> Set[str]:
-        return {k["cat_id"] for k in self._cat_to_rel[cat_id]["children"]}
+        return {k["cat_id"] for k in self._cat_to_rel[cat_id].children}
 
     def get_grandparents(self, cat_id: str) -> Set[str]:
         grandparents = set()
