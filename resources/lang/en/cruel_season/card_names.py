0b59228705e16caaa0bf6532a402f3a91bebd899
Kris <Kaylee.leo03@gmail.com>
2026-05-15 11:19:56 -0400

    formatting

--- resources/lang/en/cruel_season/card_names.py
+++ resources/lang/en/cruel_season/card_names.py
@@ -1,7 +1,7 @@
-thisdict =	{
-  "neverending_cold": "Season is permanently leaf-bare.",
-  "scarce_prey": "Fresh-kill is much harder to find.",
-  "persistent_twolegs": "Twolegs target your clan at a higher rate.",
-  "endless_strife": "A war that never ends.",
-  "blighted_herbs": "Herbs expire more quickly."
+thisdict = {
+    "neverending_cold": "Season is permanently leaf-bare.",
+    "scarce_prey": "Fresh-kill is much harder to find.",
+    "persistent_twolegs": "Twolegs target your clan at a higher rate.",
+    "endless_strife": "A war that never ends.",
+    "blighted_herbs": "Herbs expire more quickly.",
 }
