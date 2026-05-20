ca63e3e8befefaee5199e6cc60019eb03b5904d5
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-20 12:49:54 -0400

    refactor: move poi tag to common

--- scripts/models/common/points_of_interest.py
+++ scripts/models/common/points_of_interest.py
@@ -1,6 +1,28 @@
 from enum import Enum
 
 
+class PointsOfInterestTagEnum(Enum):
+    CAVE = "cave"
+    COVERED = "covered"
+    FALL_RISK = "fall_risk"
+    HOLE = "hole"
+    PREY = "prey"
+    PREY_FLYING = "prey:flying"
+    PREY_WATER = "prey:water"
+    PREY_GROUND = "prey:ground"
+    ROCKS = "rocks"
+    TAINTED = "tainted"
+    TREES = "trees"
+    TWOLEGS = "Twolegs"
+    TWOLEGS_ABANDONED = "Twolegs:abandoned"
+    TWOLEGS_PRESENT = "Twolegs:present"
+    UNSTABLE = "unstable"
+    WATER = "water"
+    WATER_STILL = "water:still"
+    WATER_FLOWING = "water:flowing"
+    WATER_OCEAN = "water:ocean"
+
+
 class PointsOfInterestGroup(Enum):
     name = "name"
     tags = "tags"
--- scripts/models/points_of_interest/points_of_interest_schema.py
+++ scripts/models/points_of_interest/points_of_interest_schema.py
@@ -1,33 +1,11 @@
 from __future__ import annotations
 
-from enum import Enum
 from typing import Dict, List, Literal, Union
 
 from pydantic import BaseModel, ConfigDict, Field, RootModel
 
+from scripts.models.common.points_of_interest import PointsOfInterestTag
 from scripts.models.common.biome import BiomeNoExclusions
-
-
-class Tag(Enum):
-    CAVE = "cave"
-    COVERED = "covered"
-    FALL_RISK = "fall_risk"
-    HOLE = "hole"
-    PREY = "prey"
-    PREY_FLYING = "prey:flying"
-    PREY_WATER = "prey:water"
-    PREY_GROUND = "prey:ground"
-    ROCKS = "rocks"
-    TAINTED = "tainted"
-    TREES = "trees"
-    TWOLEGS = "Twolegs"
-    TWOLEGS_ABANDONED = "Twolegs:abandoned"
-    TWOLEGS_PRESENT = "Twolegs:present"
-    UNSTABLE = "unstable"
-    WATER = "water"
-    WATER_STILL = "water:still"
-    WATER_FLOWING = "water:flowing"
-    WATER_OCEAN = "water:ocean"
 
 
 class PointOfInterestItem(BaseModel):
@@ -40,7 +18,7 @@ class PointOfInterestItem(BaseModel):
         ..., description="Biomes the Point of Interest belongs to."
     )
     # tags seem able to be kind of arbitrary, so this is just for autocomplete
-    tags: List[Union[Tag, str]]
+    tags: List[PointsOfInterestTag]
 
 
 class PointsOfInterestSchema(RootModel):
