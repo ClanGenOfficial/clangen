26414d4988f8758798777741dcb7f5cfa5540d06
scribblecrumb <abspence2199@gmail.com>
2026-05-26 21:09:50 -0500

    remove unused attribute

--- scripts/events_module/short/condition_events.py
+++ scripts/events_module/short/condition_events.py
@@ -354,9 +354,6 @@ class Condition_Events:
         triggered = False
 
         modify_for_war = switch_get_value(Switch.war_rel_change_type) != "rel_up"
-        mode = (
-            "expanded" if game.clan.game_mode == "cruel_season" else game.clan.game_mode
-        )
         path = (
             "condition_related.classic_injury_chance"
             if game.clan.game_mode == "classic"
