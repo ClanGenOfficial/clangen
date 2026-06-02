06b180498b604e72380b4fad5585435326dda007
scribblecrumb <abspence2199@gmail.com>
2026-06-01 19:39:40 -0500

    Update stat_dict.py

--- scripts/models/thought/stat_dict.py
+++ scripts/models/thought/stat_dict.py
@@ -3,11 +3,11 @@ from typing import List, Union
 from pydantic import BaseModel
 from pydantic_core import MISSING
 
+from scripts.models.common.all_trait import AllTrait
 from scripts.models.common.skill import Skill
-from scripts.models.common.trait import Trait
 
 
 class StatDict(BaseModel):
     skill: Union[List[Skill], MISSING] = MISSING
-    trait: Union[List[Trait], MISSING] = MISSING
+    trait: Union[List[AllTrait], MISSING] = MISSING
     must_have_both: Union[bool, MISSING] = MISSING
