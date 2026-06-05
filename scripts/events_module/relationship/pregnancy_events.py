7bb2737f54b70280dba413239db0599bf258e3b5
scribblecrumb <abspence2199@gmail.com>
2026-06-05 14:15:01 -0500

    prevent other clancats from getting pregante

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -107,7 +107,7 @@ class Pregnancy_Events:
                 # events.ceremony_accessory = True
                 return
 
-        if cat.status.is_outsider:
+        if not cat.status.alive_in_player_clan:
             return
 
         # Handle birth cooldown outside of the check_if_can_have_kits function, so it only happens once
