87a9d20a65a3d95765b16399673aac1ee60f51b8
scribblecrumb <abspence2199@gmail.com>
2026-06-05 15:22:35 -0500

    add tests for various preggo settings

--- tests/test_relation_events.py
+++ tests/test_relation_events.py
@@ -21,7 +21,7 @@ class CanHaveKits(unittest.TestCase):
         # then
         self.assertFalse(
             Pregnancy_Events.check_if_can_have_kits(
-                cat, single_parentage=True, allow_unmated=True, allow_affair=True
+                cat, allow_single_parent=True, allow_unmated=True, allow_affair=True
             )
         )
 
