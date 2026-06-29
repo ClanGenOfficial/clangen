5f761db01c974b86f37625cdfc49fa0095e0cfed
scribblecrumb <abspence2199@gmail.com>
2026-06-28 19:53:05 -0500

    adding parameter dict structures

--- scripts/events_module/parameter_dicts.py
+++ scripts/events_module/parameter_dicts.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, NotRequired
+from typing import TypedDict, NotRequired, Literal
 
 
 class StandingDict(TypedDict):
@@ -44,3 +44,46 @@ class RelationshipChangeDict(TypedDict):
     mutual: bool
     values: list[str]
     amount: int
+
+
+class RequiredReputationDict(TypedDict, total=False):
+    other_clan: Literal["ally", "neutral", "enemy"]
+    outsider: Literal["welcoming", "neutral", "hostile"]
+
+
+class ReputationChangesDict(TypedDict, total=False):
+    other_clan: int
+    outsider: int
+
+
+class DeathDict(TypedDict):
+    cats: list[str]
+    history: str
+
+
+class InjuryDict(TypedDict):
+    cats: list[str]
+    injury: list[str]
+    scar_pool_override: NotRequired[list[str]]
+    scar_history: NotRequired[str]
+    death_history: NotRequired[str]
+
+
+class LostDict(TypedDict):
+    cats: list[str]
+
+
+class SupplyDict(TypedDict):
+    type: str
+    trigger: Literal["always", "excess", "full", "adequate", "low"]
+    adjust: Literal[
+        "reduce_eighth",
+        "reduce_quarter",
+        "reduce_half",
+        "reduce_full",
+        "increase_tiny",
+        "increase_small",
+        "increase_medium",
+        "increase_large",
+        "increase_huge"
+    ]
