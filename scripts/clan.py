131b1865cd62c099fe0828297172b736d3f7b511
scribblecrumb <abspence2199@gmail.com>
2026-07-07 18:07:55 -0500

    formatter

--- scripts/clan.py
+++ scripts/clan.py
@@ -794,7 +794,6 @@ class Clan:
 
         load_pois(clan_data.get("poi", {"empty": []}))
 
-
         game.clan = Clan(
             save_id=save_id,
             display_name=clan_data.get(
@@ -806,7 +805,11 @@ class Clan:
             biome=clan_data["biome"],
             camp_bg=clan_data["camp_bg"],
             game_mode=clan_data["gamemode"],
-            cruel_cards=[c for c in clan_data.get("cruel_cards", []) if c in constants.CRUEL_CARDS_ALL],
+            cruel_cards=[
+                c
+                for c in clan_data.get("cruel_cards", [])
+                if c in constants.CRUEL_CARDS_ALL
+            ],
             self_run_init_functions=False,
         )
         game.clan.post_initialization_functions()
