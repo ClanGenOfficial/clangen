081b95fa482edc90e909169f4a5d83c5e2acb071
scribblecrumb <abspence2199@gmail.com>
2026-07-22 20:14:03 -0500

    Update patrol.py

--- scripts/events_module/patrol/patrol.py
+++ scripts/events_module/patrol/patrol.py
@@ -117,7 +117,7 @@ class Patrol:
                 return (
                     event_text_adjust(
                         Cat,
-                        self.patrol_event.intro_text,
+                        self.patrol_event.decline_text,
                         involved_cat_dict=self.involved_cats,
                         clan=game.clan,
                         other_clan=self.other_clan,
