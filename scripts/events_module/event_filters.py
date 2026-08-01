6676ebcd4f17ad1b0bedb72469daa5f7d22e0caf
scribblecrumb <abspence2199@gmail.com>
2026-08-01 11:02:41 -0500

    strip values

--- scripts/events_module/event_filters.py
+++ scripts/events_module/event_filters.py
@@ -1598,6 +1598,7 @@ def filter_relationship_type(group: list, filter_types: List[str], patrol_leader
     exclusionary_values = []
     inclusionary_values = []
     for value in filter_types:
+        value.strip()
         if "-" in value:
             exclusionary_values.append(value.replace("-", ""))
         else:
