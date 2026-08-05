6e26cf4aa2b4449e72eb40992269d0409fb1d2a9
Rusty <rusty3925@gmail.com>
2026-08-05 13:58:45 -0700

    EventLoadingAnimation: game over window title

--- scripts/ui/event_load_animation.py
+++ scripts/ui/event_load_animation.py
@@ -1,5 +1,7 @@
 import threading
 import time
+
+import i18n
 import pygame
 import pygame_gui
 
@@ -16,7 +18,7 @@ class EventLoadingAnimation(GameWindow):
 
         super().__init__(
             ui_scale(pygame.Rect(pos, (100, 100))),
-            window_display_title="Game Over",
+            window_display_title=i18n.t("windows.game_over_title"),
             object_id="#loading_window",
             back_button=False,
         )
