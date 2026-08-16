cb6eec7ccec65dbbe8285989f402b913d214d1a1
scribblecrumb <abspence2199@gmail.com>
2026-08-15 20:18:42 -0500

    Update test_relation_events.py

--- tests/test_relation_events.py
+++ tests/test_relation_events.py
@@ -168,7 +168,7 @@ class SameSexAdoptions(unittest.TestCase):
 
 
 class Pregnancy(unittest.TestCase):
-    @patch("scripts.events_module.relationship.pregnancy_events.check_if_can_have_kits")
+    @patch("scripts.events_module.pregnancy.check_parents.check_if_can_have_kits")
     def test_single_cat_female(self, check_if_can_have_kits):
         # given
         clan = Clan(save_id="clan")
@@ -184,7 +184,7 @@ class Pregnancy(unittest.TestCase):
         # then
         self.assertIn(cat.ID, clan.pregnancy_data.keys())
 
-    @patch("scripts.events_module.relationship.pregnancy_events.check_if_can_have_kits")
+    @patch("scripts.events_module.pregnancy.check_parents.check_if_can_have_kits")
     def test_pair(self, check_if_can_have_kits):
         # given
         clan = Clan(save_id="clan")
