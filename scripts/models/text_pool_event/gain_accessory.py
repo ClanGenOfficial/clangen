32e5d321ef33de14b863d5649a22e06e22466ff1
scribblecrumb <abspence2199@gmail.com>
2026-08-23 22:59:39 -0500

    Update gain_accessory.py

--- scripts/models/text_pool_event/gain_accessory.py
+++ scripts/models/text_pool_event/gain_accessory.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from typing import Union
-
 from pydantic import BaseModel, ConfigDict, Field
 from pydantic_core import MISSING
 
 from scripts.models.common.gather_cat import GatherCat
-from scripts.models.common.status import Status
 
 
 class GainAccessory(BaseModel):
