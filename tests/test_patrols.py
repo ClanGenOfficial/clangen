cedd421bae8298e0879242605cdbf6c6f8561712
scribblecrumb <115410010+scribblecrumb@users.noreply.github.com>
2026-09-03 20:43:28 -0500

    [BUGFIX] Patrol debug and s_c handling (#5949)
    * add eq
    
    * improve romance debug
    
    * let p_l be in interactable cats
    
    * add some helpful s_c tests
    
    * Update transition.schema.json
    
    * appease formatter
    
    * Update patrol.py
    
    * Update patrol.py
    
    * fixing test

--- tests/test_patrols.py
+++ tests/test_patrols.py
@@ -309,6 +309,59 @@ class TestInvolvedCats(unittest.TestCase):
             msg=f"p_l and outcome s_c can't match.",
         )
 
+    def test_sc_overall_patrol(self):
+        war1 = TestCatFactory.create_cat(rank=CatRank.WARRIOR)
+        war2 = TestCatFactory.create_cat(rank=CatRank.WARRIOR)
+
+        with self.subTest(
+            "Test if a single s_c0 cat can be found for the patrol overall."
+        ):
+            patrol = PatrolEvent(
+                event_id="test",
+                types=["hunting"],
+                intro_strings=["test"],
+                decline_strings=["test"],
+                involved_cats={
+                    "s_c0": InvolvedCatDict(prior_abbreviation=["any"]),
+                },
+                success_outcomes=[{"strings": ["test"]}],
+                fail_outcomes=[{"strings": ["test"]}],
+            )
+            self.patrol_class.patrol_event = patrol
+            self.patrol_class._add_patrol_cats([war1])
+            self.patrol_class._get_valid_patrol([patrol])
+
+            self.assertEqual(
+                self.patrol_class.involved_cats["p_l"],
+                self.patrol_class.involved_cats["p_l"],
+                msg=f"p_l: {self.patrol_class.involved_cats['p_l']} and s_c0: {self.patrol_class.involved_cats['s_c0']} don't match when they should.",
+            )
+
+        with self.subTest(
+            "Test if two different s_c cats can be found for the patrol overall."
+        ):
+            patrol = PatrolEvent(
+                event_id="test",
+                types=["hunting"],
+                intro_strings=["test"],
+                decline_strings=["test"],
+                involved_cats={
+                    "s_c0": InvolvedCatDict(prior_abbreviation=["any"]),
+                    "s_c1": InvolvedCatDict(prior_abbreviation=["-s_c0"]),
+                },
+                success_outcomes=[{"strings": ["test"]}],
+                fail_outcomes=[{"strings": ["test"]}],
+            )
+            self.patrol_class.patrol_event = patrol
+            self.patrol_class._add_patrol_cats([war1, war2])
+            self.patrol_class._get_valid_patrol([patrol])
+
+            self.assertNotEqual(
+                self.patrol_class.involved_cats["s_c0"],
+                self.patrol_class.involved_cats["s_c1"],
+                msg=f"s_c0: {self.patrol_class.involved_cats['s_c0']} and s_c1: {self.patrol_class.involved_cats['s_c1']} match when they shouldn't.",
+            )
+
 
 class TestOutcomeExecution(unittest.TestCase):
     def setUp(self):
@@ -348,7 +401,10 @@ class TestOutcomeExecution(unittest.TestCase):
             )
 
             self.patrol_class._add_patrol_cats([war1])
-            self.patrol_class._get_valid_patrol([patrol])
+            self.patrol_class.patrol_event = self.patrol_class._get_valid_patrol(
+                [patrol]
+            )
+            self.patrol_class._create_needed_cats()
             self.patrol_class._check_outcome_constraints(
                 patrol.success_outcomes[0], "success"
             )
@@ -377,7 +433,10 @@ class TestOutcomeExecution(unittest.TestCase):
                 fail_outcomes=[{"strings": ["test"]}],
             )
             self.patrol_class._add_patrol_cats([war1])
-            self.patrol_class._get_valid_patrol([patrol])
+            self.patrol_class.patrol_event = self.patrol_class._get_valid_patrol(
+                [patrol]
+            )
+            self.patrol_class._create_needed_cats()
             self.patrol_class._check_outcome_constraints(
                 patrol.success_outcomes[0], "success"
             )
