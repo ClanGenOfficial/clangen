5d518d67e46c022c39a9752e3d053d695a16efee
keyraven <118307471+keyraven@users.noreply.github.com>
2026-09-05 19:03:23 -0600

    Formatting

--- scripts/events_module/consequences.py
+++ scripts/events_module/consequences.py
@@ -368,11 +368,12 @@ def create_new_cat_block(
                         name = new_prefix
                     chosen_cat.name.prefix = name
                     chosen_cat.name.give_suffix(
-                        eyes = chosen_cat.pelt.eye_colour, 
-                        color = chosen_cat.pelt.colour,
-                        pelt = chosen_cat.pelt.name,
+                        eyes=chosen_cat.pelt.eye_colour,
+                        color=chosen_cat.pelt.colour,
+                        pelt=chosen_cat.pelt.name,
                         biome=game.clan.biome,
-                        tortie_pattern= cat.pelt.tortie_pattern)
+                        tortie_pattern=cat.pelt.tortie_pattern,
+                    )
                 else:  # completely new name
                     chosen_cat.name.give_prefix(
                         eyes=chosen_cat.pelt.eye_colour,
