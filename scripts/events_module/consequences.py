062995f8914cea8b4716b1b78b4bf2baa4d0a4f3
keyraven <118307471+keyraven@users.noreply.github.com>
2026-09-14 21:27:10 -0600

    [BUGFIX] Fix Baby Kittens Joining from Leader's Den (#6059)
    * Fix Baby Kittens Joining from Leader's Den
    
    * Formatting.

--- scripts/events_module/consequences.py
+++ scripts/events_module/consequences.py
@@ -372,7 +372,7 @@ def create_new_cat_block(
                         colour=chosen_cat.pelt.colour,
                         pelt=chosen_cat.pelt.name,
                         biome=game.clan.biome,
-                        tortie_pattern=cat.pelt.tortie_pattern,
+                        tortie_pattern=chosen_cat.pelt.tortie_pattern,
                     )
                 else:  # completely new name
                     chosen_cat.name.give_prefix(
@@ -386,7 +386,7 @@ def create_new_cat_block(
                         colour=chosen_cat.pelt.colour,
                         pelt=chosen_cat.pelt.name,
                         biome=game.clan.biome,
-                        tortie_pattern=cat.pelt.tortie_pattern,
+                        tortie_pattern=chosen_cat.pelt.tortie_pattern,
                     )
 
             new_cats = [chosen_cat]
