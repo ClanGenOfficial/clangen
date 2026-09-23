fa1738acf5da33da3e09e5a70e039852fd2de4ce
elusiveFalsehoods <118483868+elusiveFalsehoods@users.noreply.github.com>
2026-09-23 16:19:48 -0400

    Update create_new_cat.py

--- scripts/events_module/patrol/create_new_cat.py
+++ scripts/events_module/patrol/create_new_cat.py
@@ -159,12 +159,6 @@ def updated_create_new_cat(
             if adoptive_parents
             else None,
         )
-        # check if kittypets get collar
-        if created_cat.status.social == CatSocial.KITTYPET and bool(getrandbits(1)):
-            created_cat.pelt.accessory = (
-                *created_cat.pelt.accessory,
-                choice(created_cat.pelt.collar_accessories),
-            )
 
         # MATES
         _assign_mates(created_cat, involved_cats, option_dict)
@@ -274,7 +268,7 @@ def _assign_name(created_cat: Cat):
         # give kittypets a kittypet name
         if created_cat.status.social == CatSocial.KITTYPET:
             weights = constants.CONFIG["cat_name_controls"]["kittypet"]
-            # check if the kittypets come with a pretty acc
+            # check if the kittypets come with a collar
             if bool(getrandbits(1)):
                 created_cat.pelt.accessory = (
                     *created_cat.pelt.accessory,
